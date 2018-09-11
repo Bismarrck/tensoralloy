8b054cbc84262cc6408c81c3a9525057fb5cf539
Bismarrck <Bismarrck@me.com>
2018-09-11 20:35:15 +0800

    `pairwise_dist` now supports computing pairwise distances of 3D matrices using `einsum` scheme.

--- utilities.py
+++ utilities.py
@@ -28,34 +28,70 @@ def cutoff(r: tf.Tensor, rc: float, name=None):
         return math_ops.multiply(z, 0.5, name=name)
 
 
-def pairwise_dist(xx, yy, name=None):
+def pairwise_dist(xx, yy=None, name=None):
     """
     Computes the pairwise distances between each elements of xx and yy.
 
     Parameters
     ----------
     xx : tf.Tensor
-        A float32 tensor of shape `[n, d]`.
+        A tensor of shape `[m, d]` or `[b, m, d]`.
     yy : tf.Tensor
-        A float32 tensor of shape `[m, d]`.
+        A tensor of shape `[n, d]` or `[b, n, d]`.
     name : str
-        The name of this op.
+        A `str` as the name of this op.
 
     Returns
     -------
     D : tf.Tensor
-        A float32 tensor of shape `[m, n]` as the pairwise distances.
+        A tensor of shape `[m, n]` or `[b, m, n]` as the pairwise distances.
+
+    See Also
+    --------
+    https://stackoverflow.com/questions/39822276
+    https://gist.github.com/mbsariyildiz/34cdc26afb630e8cae079048eef91865
 
     """
     with ops.name_scope(name, 'pairwise_dist', [xx, yy]) as name:
-        # squared norms of each row in A and B
-        na = tf.reduce_sum(tf.square(xx), 1)
-        nb = tf.reduce_sum(tf.square(yy), 1)
+        if yy is None:
+            yy = xx
+        if len(xx.shape) == 2:
+            assert xx.shape[1] == yy.shape[1]
+        elif len(xx.shape) == 3:
+            assert xx.shape[0] == yy.shape[0]
+        else:
+            raise ValueError("The rank of the input matrices must be 2 or 3")
 
-        # na as a row and nb as a co"lumn vectors
-        na = tf.reshape(na, [-1, 1])
-        nb = tf.reshape(nb, [1, -1])
+        dtype = xx.dtype
+        two = tf.constant(2.0, dtype=dtype, name='two')
+        zero = tf.constant(0.0, dtype=dtype, name='zero')
+
+        if len(xx.shape) == 2:
+            # squared norms of each row in A and B
+            na = tf.reduce_sum(tf.square(xx), 1)
+            nb = tf.reduce_sum(tf.square(yy), 1)
+
+            # na as a row and nb as a column vectors
+            na = tf.reshape(na, [-1, 1])
+            nb = tf.reshape(nb, [1, -1])
+
+            # xy = xx * yy.T
+            xy = tf.matmul(xx, yy, transpose_a=False, transpose_b=True)
+        else:
+            # Get the batch size
+            batch_size = xx.shape[0]
+
+            # squared norms of each row in A and B
+            na = tf.reduce_sum(tf.square(xx), 2, keep_dims=True)
+            nb = tf.reduce_sum(tf.square(yy), 2, keep_dims=True)
+
+            # na as a row and nb as a column vectors
+            na = tf.reshape(na, [batch_size, -1, 1])
+            nb = tf.reshape(nb, [batch_size, 1, -1])
+
+            # Use einsum to compute matrix multiplications
+            xy = tf.einsum('ijk,ilk->ijl', na, nb, name='einsum')
 
         # return pairwise euclidead difference matrix
-        zz = tf.maximum(na - 2 * tf.matmul(xx, yy, False, True) + nb, 0.0)
+        zz = tf.maximum(na - tf.multiply(two, xy, name='2xy') + nb, zero)
         return tf.sqrt(zz, name=name)
