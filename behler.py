eba713986ad8b40154b0dab900a92a7576949c3f
Bismarrck <Bismarrck@me.com>
2018-10-12 13:19:01 +0800

    `radial_function` and `angular_function` are almost done. The `v2g_map` for both shall be adjusted.

--- behler.py
+++ behler.py
@@ -8,9 +8,10 @@ import tensorflow as tf
 import numpy as np
 from ase import Atoms
 from ase.neighborlist import neighbor_list
-from utils import cutoff, pairwise_dist
+from utils import cutoff
 from itertools import chain
 from collections import namedtuple
+from sklearn.model_selection import ParameterGrid
 from typing import List
 
 __author__ = 'Xin Chen'
@@ -260,84 +261,115 @@ def build_angular_v2g_map(atoms: Atoms, rmap: RadialMap, kbody_terms):
                       jkSlist=jkS)
 
 
-def radial_function(R: tf.Tensor, rc: float, g: tf.Variable, etas,
-                    ilist: tf.Tensor, jlist: tf.Tensor, Slist: tf.Tensor,
-                    v2g_map: tf.Tensor, cell: tf.Tensor):
+def radial_function(R: tf.Tensor, rc: float, g: tf.Variable, v2g_map, cell,
+                    etas, ilist, jlist, Slist):
     """
     The implementation of Behler's radial symmetry function for a single
     structure.
     """
     with tf.name_scope("G2"):
-        with tf.name_scope("mic"):
+
+        with tf.name_scope("constants"):
+            rc2 = tf.constant(rc ** 2, dtype=tf.float64, name='rc2')
+            etas = tf.constant(etas, dtype=tf.float64, name='etas')
+
+        with tf.name_scope("rij"):
             Ri = tf.gather(R, ilist, name='Ri')
             Rj = tf.gather(R, jlist, name='Rj')
             Dlist = Rj - Ri + tf.matmul(Slist, cell)
-            dlist = tf.norm(Dlist, axis=1, name='dlist')
-        rc2 = tf.constant(rc**2, dtype=tf.float64, name='rc2')
-        r = tf.identity(dlist, name='r')
-        r2 = tf.square(r, name='r2')
-        fc_r = cutoff(r, rc=rc, name='fc_r')
-        etas = tf.constant(etas, dtype=tf.float64, name='etas')
-        v = tf.div(r2, rc2, name='div')
-        v = tf.tensordot(etas, v, 0, name='tensordot')
-        v = tf.exp(tf.negative(v, name='neg'), name='exp')
-        v = tf.multiply(v, fc_r, name='vfcr')
-        v = tf.reshape(v, [-1], name='flatten')
-        return tf.scatter_nd_add(g, v2g_map, v)
+            r = tf.norm(Dlist, axis=1, name='r')
+            r2 = tf.square(r, name='r2')
+            r2c = tf.div(r2, rc2, name='div')
+
+        with tf.name_scope("fc"):
+            fc_r = cutoff(r, rc=rc, name='fc_r')
+
+        with tf.name_scope("g"):
+            v = tf.exp(-tf.tensordot(etas, r2c), name='exp') * fc_r
+            v = tf.reshape(v, [-1], name='flatten')
+            return tf.scatter_nd_add(g, v2g_map, v)
 
 
-def angular_function(R: tf.Tensor, rc: float, indices: dict, eta_list: list,
-                     gamma_list: list, zeta_list: list, v_to_g_map):
+def angular_function(R: tf.Tensor, rc: float, g: tf.Variable, v2g_map, cell,
+                     grid: ParameterGrid, ij, ik, jk, ijS, ikS, jkS):
     """
     The implementation of Behler's angular symmetry function for a single
     structure.
+
+    Parameters
+    ----------
+    R : tf.Tensor
+        A `float32` or `float64` tensor of shape `[N, 3]` as the cartesian
+        coordinates of a structure where N is the number of atoms.
+    rc : float
+        A float or float tensor as the cutoff radius.
+    g : tf.Variable
+        A `float32` or `float64` tensor of shape `[N, n_dims]` as the computed
+        atomic features.
+    v2g_map : tf.Tensor
+
+    cell : tf.Tensor
+        A `float32` or `float64` tensor of shape `[3, 3]` as the unit cell of
+        the structure.
+    grid : ParameterGrid
+        The
+
     """
-    one = tf.constant(1.0, dtype=tf.float64)
-    half = tf.constant(0.5, dtype=tf.float64)
-    two = tf.constant(2.0, dtype=tf.float64)
-    ndim = len(eta_list) * len(gamma_list) * len(zeta_list)
-    N = R.shape[0]
+    with tf.name_scope("G4"):
 
-    r = pairwise_dist(R, R, name='pdist')
-    r2 = tf.square(r)
-    rc2 = tf.constant(rc ** 2, dtype=tf.float64, name='rc2')
+        with tf.name_scope("constants"):
+            one = tf.constant(1.0, dtype=tf.float64)
+            two = tf.constant(2.0, dtype=tf.float64)
+            rc2 = tf.constant(rc**2, dtype=tf.float64, name='rc2')
+            n_dims = tf.constant(len(grid), name='n_dims')
 
-    r_ij = tf.gather_nd(r, indices['ij'], name='ij3')
-    r_ik = tf.gather_nd(r, indices['ik'], name='ik3')
-    r_jk = tf.gather_nd(r, indices['jk'], name='jk3')
+        with tf.name_scope("Rij"):
+            Ri_ij = tf.gather(R, ij[:, 0])
+            Rj_ij = tf.gather(R, ij[:, 1])
+            D_ij = Rj_ij - Ri_ij + tf.matmul(ijS, cell)
+            r_ij = tf.norm(D_ij, axis=1)
 
-    # Compute $\cos{(\theta_{ijk})}$ using the cosine formula
-    b2c2a2 = tf.square(r_ij) + tf.square(r_ik) - tf.square(r_jk)
-    bc = tf.multiply(r_ij, r_ik, name='bc')
-    bc = tf.multiply(two, bc, name='2bc')
-    cos_theta = tf.div(b2c2a2, bc, name='cos_theta')
+        with tf.name_scope("Rik"):
+            Ri_ik = tf.gather(R, ik[:, 0])
+            Rk_ik = tf.gather(R, ik[:, 1])
+            D_ik = Rk_ik - Ri_ik + tf.matmul(ikS, cell)
+            r_ik = tf.norm(D_ik, axis=1)
 
-    # Compute the damping term: $f_c(r_{ij}) * f_c(r_{ik}) * f_c(r_{jk})$
-    fc_r_ij = cutoff(r_ij, rc, name='fc_r_ij')
-    fc_r_ik = cutoff(r_ik, rc, name='fc_r_ik')
-    fc_r_jk = cutoff(r_jk, rc, name='fc_r_jk')
-    fc_r_ijk = fc_r_ij * fc_r_ik * fc_r_jk
+        with tf.name_scope("Rik"):
+            Rj_jk = tf.gather(R, jk[:, 0])
+            Rk_jk = tf.gather(R, jk[:, 1])
+            D_jk = Rk_jk - Rj_jk + tf.matmul(jkS, cell)
+            r_jk = tf.norm(D_jk, axis=1)
 
-    # Compute $R_{ij}^{2} + R_{ik}^{2} + R_{jk}^{2}$
-    r2_ij = tf.gather_nd(r2, indices['ij'], name='r2_ij')
-    r2_ik = tf.gather_nd(r2, indices['ik'], name='r2_ik')
-    r2_jk = tf.gather_nd(r2, indices['jk'], name='r2_jk')
-    r2_sum = r2_ij + r2_ik + r2_jk
+        # Compute $\cos{(\theta_{ijk})}$ using the cosine formula
+        with tf.name_scope("cosine"):
+            upper = tf.square(r_ij) + tf.square(r_ik) - tf.square(r_jk)
+            lower = tf.multiply(r_ij, r_ik, name='bc')
+            lower = tf.multiply(two, lower, name='2bc')
+            theta = tf.div(upper, lower, name='theta')
 
-    g = tf.Variable(tf.zeros((N, ndim), dtype=tf.float64), trainable=False)
-    group = []
-    row = 0
-    for eta in eta_list:
-        for gamma in gamma_list:
-            for zeta in zeta_list:
-                gamma_ = tf.constant(gamma, dtype=tf.float64)
-                eta_ = tf.constant(eta, dtype=tf.float64)
-                zeta_ = tf.constant(zeta, dtype=tf.float64)
-                v = one + tf.multiply(gamma_, cos_theta)
-                v = tf.pow(v, zeta_, name='pow')
-                v = tf.pow(two, one - zeta_) * v
-                v = v * tf.exp(-eta_ * r2_sum / rc2) * fc_r_ijk
-                group.append(tf.scatter_nd_add(g, v_to_g_map[row], v))
-                row += 1
-    with tf.control_dependencies(group):
-        return tf.multiply(half, g)
+        # Compute the damping term: $f_c(r_{ij}) * f_c(r_{ik}) * f_c(r_{jk})$
+        with tf.name_scope("fc"):
+            fc_r_ij = cutoff(r_ij, rc, name='fc_r_ij')
+            fc_r_ik = cutoff(r_ik, rc, name='fc_r_ik')
+            fc_r_jk = cutoff(r_jk, rc, name='fc_r_jk')
+            fc_r_ijk = fc_r_ij * fc_r_ik * fc_r_jk
+
+        # Compute $R_{ij}^{2} + R_{ik}^{2} + R_{jk}^{2}$
+        with tf.name_scope("r2"):
+            r2_ij = tf.square(r_ij, name='r2_ij')
+            r2_ik = tf.square(r_ij, name='r2_ik')
+            r2_jk = tf.square(r_ij, name='r2_jk')
+            r2 = r2_ij + r2_ik + r2_jk
+            r2c = tf.div(r2, rc2, name='r2_rc2')
+
+        with tf.name_scope("g"):
+            for row, params in enumerate(grid):
+                with tf.name_scope("p{}".format(row)):
+                    gamma = params['gamma']
+                    zeta = params['zeta']
+                    eta = params['eta']
+                    c = (one + gamma * theta)**zeta * two**(1.0 - zeta)
+                    v = c * tf.exp(-eta * r2c) * fc_r_ijk
+                    g = tf.scatter_nd_add(g, v2g_map, v)
+            return g
