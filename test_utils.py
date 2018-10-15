240e2c3e4ca3572286d2cd1d5256bafb2fec5e5e
Bismarrck <Bismarrck@me.com>
2018-10-15 14:07:11 +0800

    Add an unit test.

---
+++ test_utils.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""
+This module defines unit tests for functions in `utils`.
+"""
+from __future__ import print_function, absolute_import
+
+import tensorflow as tf
+import numpy as np
+import utils
+from nose import main
+from nose.tools import assert_less
+
+tf.enable_eager_execution()
+
+__author__ = 'Xin Chen'
+__email__ = 'Bismarrck@me.com'
+
+
+def test_batch_gather_positions():
+    R = np.arange(18.0, dtype=np.float64).reshape((2, 3, 3))
+    indices = np.array([[0, 0, 1, 1], [1, 2, 2, 1]])
+    results = np.zeros((2, 4, 3), dtype=np.float64)
+    for i, row in enumerate(indices):
+        results[i] = R[i][row]
+    g = utils.batch_gather_positions(R, indices).numpy()
+    assert_less(np.abs(g - results).max(), 1e-8)
+
+
+if __name__ == "__main__":
+    main()
