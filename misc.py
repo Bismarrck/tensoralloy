dc3bc93d61344bbc4366eca7924810cd9111ec47
Bismarrck <Bismarrck@me.com>
2018-10-23 17:43:54 +0800

    Add a new function: `check_path`. Add a new constant: RANDOM_STATE.

--- misc.py
+++ misc.py
@@ -5,9 +5,15 @@ This module defines miscellaneous functions.
 from __future__ import print_function, absolute_import
 
 from unittest import SkipTest
+from os.path import dirname, isdir
+from os import makedirs
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+
+# The random seed.
+RANDOM_STATE = 611
 
 
 def skip(func):
@@ -18,3 +24,12 @@ def skip(func):
         raise SkipTest("Test %s is skipped" % func.__name__)
     _.__name__ = func.__name__
     return _
+
+
+def check_path(path):
+    """ Make sure the given path is accessible. """
+    dst = dirname(path)
+    if not isdir(dst):
+        makedirs(dst)
+    return path
+
