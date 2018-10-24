e8a0b1aadbcb9e48ad2db9a5d1bd8969eba247ab
Bismarrck <Bismarrck@me.com>
2018-10-24 16:02:39 +0800

    Add a new class `AttributeDict`.

--- misc.py
+++ misc.py
@@ -17,6 +17,32 @@ __email__ = 'Bismarrck@me.com'
 RANDOM_STATE = 611
 
 
+class AttributeDict(dict):
+    """
+    A subclass of `dict` with attribute-style access.
+    """
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __repr__(self):
+        if self.keys():
+            m = max(map(len, list(self.keys()))) + 1
+            return '\n'.join([k.rjust(m) + ': ' + repr(v)
+                              for k, v in sorted(self.items())])
+        else:
+            return self.__class__.__name__ + "()"
+
+    def __dir__(self):
+        return list(self.keys())
+
+
 class Defaults:
     """
     A dataclass storing default parameters.
