fb897d9ac68ba723802a06d2e0f732732f75c6f5
Bismarrck <Bismarrck@me.com>
2018-10-25 16:36:27 +0800

    A simple trainble model is implemented.

--- model.py
+++ model.py
@@ -5,9 +5,11 @@ This module defines the model function.
 from __future__ import print_function, absolute_import
 
 import tensorflow as tf
+import time
+from os.path import join
 from tensorflow.contrib.layers import xavier_initializer
 from dataset import Dataset
-from misc import Defaults
+from misc import Defaults, check_path
 from ase.db import connect
 
 __author__ = 'Xin Chen'
@@ -18,8 +20,9 @@ dataset = Dataset(connect('datasets/qm7.db'), name='qm7', k_max=2)
 if not dataset.load_tfrecords('datasets/qm7'):
     dataset.to_records('datasets/qm7', test_size=1165, verbose=True)
 
-batch_size = 10
+batch_size = 60
 hidden_sizes = [64, 32]
+learning_rate = 0.001
 graph = tf.Graph()
 
 with graph.as_default():
@@ -33,28 +36,68 @@ with graph.as_default():
     y_true = examples.y_true
     y_true.set_shape([batch_size, ])
 
-    xavier = xavier_initializer(seed=Defaults.seed)
+    kernel_initializer = xavier_initializer(
+        seed=Defaults.seed, dtype=tf.float64)
+    bias_initializer = tf.zeros_initializer(dtype=tf.float64)
 
     with tf.name_scope("ANN"):
         outputs = []
         for i, element in enumerate(dataset.descriptor.elements):
             with tf.variable_scope(element):
-                x = tf.cast(splits[i], tf.float32)
+                x = tf.identity(splits[i], name='x')
                 for j in range(len(hidden_sizes)):
                     with tf.variable_scope('Hidden{}'.format(j + 1)):
                         x = tf.layers.conv1d(
                             inputs=x, filters=hidden_sizes[j], kernel_size=1,
                             strides=1, activation=tf.nn.leaky_relu,
-                            use_bias=True, kernel_initializer=xavier,
+                            use_bias=True,
+                            kernel_initializer=kernel_initializer,
+                            bias_initializer=bias_initializer,
                             name='1x1Conv{}'.format(i + 1))
                 yi = tf.layers.conv1d(inputs=x, filters=1, kernel_size=1,
                                       strides=1, use_bias=False,
-                                      kernel_initializer=xavier, name='Output')
+                                      kernel_initializer=kernel_initializer,
+                                      name='Output')
                 outputs.append(tf.squeeze(yi, axis=2, name='ae'))
 
     with tf.name_scope("Energy"):
         y_atomic = tf.concat(outputs, axis=1, name='y_atomic')
-        y_total = tf.reduce_sum(y_atomic, axis=1, keep_dims=False, name='y')
+        y_total = tf.reduce_sum(y_atomic, axis=1, keepdims=False, name='y')
+
+    with tf.name_scope("Penalty"):
+        for var in tf.trainable_variables():
+            if 'bias' in var.op.name:
+                continue
+            l2 = tf.nn.l2_loss(var, name=var.op.name + "/l2")
+            tf.add_to_collection('l2_losses', l2)
+        l2_loss = tf.add_n(tf.get_collection('l2_losses'), name='l2_sum')
+        alpha = tf.convert_to_tensor(0.001, dtype=tf.float64, name='alpha')
+        l2 = tf.multiply(l2_loss, alpha, name='l2')
 
     with tf.name_scope("Loss"):
-        rmse = tf.metrics.root_mean_squared_error(y_true, y_total, name='rmse')
+        mse = tf.reduce_mean(tf.squared_difference(y_true, y_total), name='mse')
+        rmse = tf.sqrt(mse, name='rmse')
+        loss = tf.add(rmse, l2, name='loss')
+        mae = tf.reduce_mean(tf.abs(y_true - y_total), name='mae')
+
+    with tf.name_scope("Optimization"):
+        global_step = tf.train.get_or_create_global_step(graph=graph)
+        train_op = tf.train.AdamOptimizer(
+            learning_rate).minimize(loss, global_step)
+
+    train_dir = check_path('datasets/qm7/train')
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+
+        tf.global_variables_initializer().run()
+
+        tic = time.time()
+        for step in range(10000):
+            _, step_loss, step_mae = sess.run([train_op, loss, mae])
+            if step and step % 100 == 0:
+                speed = (step + 1) / (time.time() - tic)
+                print('step: {:5d}, loss: {:8.5f}, mae: {:8.5f} speed: {:.1f}'.format(
+                    step, step_loss, step_mae, speed))
+            if step and step % 1000 == 0:
+                saver.save(sess, join(train_dir, dataset.name), global_step)
