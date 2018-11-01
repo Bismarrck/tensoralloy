6b08d01e49c9cacc92780b045571bf789211d073
Bismarrck <Bismarrck@me.com>
2018-11-01 09:29:41 +0800

    Implement a specific `ConfigParser` for this project.

---
+++ config.py
@@ -0,0 +1,152 @@
+# coding=utf-8
+"""
+This module defines the `ConfigParser` for this project.
+"""
+from __future__ import print_function, absolute_import
+
+import configparser
+from ase.db import connect
+from os.path import splitext, basename
+from typing import Callable
+
+from misc import Defaults, AttributeDict
+from dataset import Dataset, TrainableProperty
+from nn import AtomicNN
+
+__author__ = 'Xin Chen'
+__email__ = 'Bismarrck@me.com'
+
+
+def _parse_list(dtype: Callable, x: str):
+    """
+    A helper function returning a list of `dtype` objects from a comma-seprated
+    string.
+    """
+    if x is None or x.strip() == '':
+        return None
+    return [dtype(e.strip()) for e in x.strip().split(',')]
+
+
+class ConfigParser(configparser.ConfigParser):
+    """
+    A specific implementation of `configparser.ConfigParser`.
+    """
+
+    def __init__(self, filename):
+        """
+        Initialization method.
+
+        Parameters
+        ----------
+        filename : str
+            The config file to read.
+
+        """
+        super(ConfigParser, self).__init__(
+            converters={
+                'floats': lambda x: _parse_list(float, x),
+                'ints': lambda x: _parse_list(int, x)})
+        with open(filename) as fp:
+            self._read(fp, filename)
+        self._hparams = self._read_hyperparams()
+        self._dataset = self._read_dataset()
+        self._nn = self._build_nn()
+
+    @property
+    def dataset(self):
+        """
+        Return the `Dataset` intialized from this config.
+        """
+        return self._dataset
+
+    @property
+    def nn(self):
+        """
+        Return the `AtomicNN` initialized from this config.
+        """
+        return self._nn
+
+    @property
+    def hyper_params(self):
+        """
+        Return a dict of hyper parameters.
+        """
+        return self._hparams
+
+    def _read_dataset(self):
+        """
+        Initialize a `Dataset`.
+        """
+        section = 'dataset'
+        filename = self[section]['sqlite3']
+        descriptor = self[section]['descriptor']
+
+        if descriptor != 'behler':
+            raise NotImplementedError(
+                "Only Behler's Symmetry Function is implemented at this time!")
+
+        database = connect(filename)
+        name = self[section].get('name', splitext(basename(filename))[0])
+        rc = self[section].getfloat('rc', Defaults.rc)
+        k_max = self[section].getint('k_max', Defaults.k_max)
+
+        section = 'behler'
+        eta = self[section].getfloats('eta', Defaults.eta)
+        beta = self[section].getfloats('beta', Defaults.beta)
+        gamma = self[section].getfloats('gamma', Defaults.gamma)
+        zeta = self[section].getfloats('zeta', Defaults.zeta)
+
+        dataset = Dataset(database=database, name=name, k_max=k_max, rc=rc,
+                          eta=eta, beta=beta, gamma=gamma, zeta=zeta)
+
+        section = 'tfrecords'
+        test_size = self[section].getint('test_size', 1000)
+        tfrecords_dir = self[section].get('tfrecords_dir', '.')
+        parallel = self[section].getboolean('build_in_parallel', True)
+        if not dataset.load_tfrecords(tfrecords_dir):
+            dataset.to_records(
+                tfrecords_dir, test_size=test_size, parallel=parallel)
+        return dataset
+
+    def _build_nn(self) -> AtomicNN:
+        """
+        Build an `AtomicNN` with the given config and dataset.
+        """
+        section = 'nn'
+
+        l2_weight = self[section].getfloat('l2_weight', 0.0)
+        activation = self[section].get('activation', 'leaky_relu')
+        forces = TrainableProperty.forces in self._dataset.trainable_properties
+
+        elements = self._dataset.descriptor.elements
+        hidden_sizes = {}
+        for element in elements:
+            hidden_sizes[element] = self[section].getints(
+                element, Defaults.hidden_sizes)
+
+        nn = AtomicNN(elements=elements, hidden_sizes=hidden_sizes,
+                      activation=activation, l2_weight=l2_weight, forces=forces)
+        return nn
+
+    def _read_hyperparams(self):
+        """
+        Read hyper parameters.
+        """
+        section = 'opt'
+
+        method = self[section].get('optimizer', 'adam')
+        learning_rate = self[section].getfloat(
+            'learning_rate', Defaults.learning_rate)
+
+        decay_function = self[section].get('decay_function', None)
+        decay_rate = self[section].getfloat('decay_rate', None)
+        decay_steps = self[section].getint('decay_steps', None)
+        staircase = self[section].getboolean('staircase', False)
+
+        hparams = AttributeDict(method=method,
+                                learning_rate=learning_rate,
+                                decay_function=decay_function,
+                                decay_steps=decay_steps,
+                                decay_rate=decay_rate,
+                                staircase=staircase)
+        return hparams
--- train_model.py
+++ train_model.py
@@ -5,116 +5,12 @@ This module is used to train the model
 from __future__ import print_function, absolute_import
 
 import tensorflow as tf
-from ase.db import connect
-from os.path import splitext, basename
 from argparse import ArgumentParser
-from configparser import ConfigParser
-from typing import Callable
-
-from dataset import Dataset, TrainableProperty
-from nn import AtomicNN
-from misc import Defaults, AttributeDict
+from config import ConfigParser
 
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
-
-
-def _parse_list(dtype: Callable, list_str: str):
-    """
-    A helper function returning a list of `dtype` objects from a comma-seprated
-    string.
-    """
-    if list_str is None or list_str.strip() == '':
-        return None
-    return [dtype(e.strip()) for e in list_str.strip().split(',')]
-
-
-def get_dataset_from_config(config: ConfigParser) -> Dataset:
-    """
-    Initialize a `Dataset` with the given config.
-    """
-    section = 'dataset'
-    filename = config.get(section, 'sqlite3')
-    descriptor = config.get(section, 'descriptor')
-
-    if descriptor != 'behler':
-        raise NotImplementedError(
-            "Only Behler's Symmetry Function is implemented at this time!")
-
-    database = connect(filename)
-    name = config.get(section, 'name', fallback=splitext(basename(filename))[0])
-    rc = config.getfloat(section, 'rc', fallback=None)
-    k_max = config.getint(section, 'k_max', fallback=2)
-
-    section = 'behler'
-    eta = _parse_list(float, config.get(section, 'eta', fallback=None))
-    beta = _parse_list(float, config.get(section, 'beta', fallback=None))
-    gamma = _parse_list(float, config.get(section, 'gamma', fallback=None))
-    zeta = _parse_list(float, config.get(section, 'zeta', fallback=None))
-
-    dataset = Dataset(database=database, name=name, k_max=k_max, rc=rc, eta=eta,
-                      beta=beta, gamma=gamma, zeta=zeta)
-
-    section = 'tfrecords'
-    test_size = config.getint(section, 'test_size', fallback=1000)
-    tfrecords_dir = config.get(section, 'tfrecords_dir', fallback='.')
-    parallel = config.getboolean(section, 'build_in_parallel', fallback=True)
-    if not dataset.load_tfrecords(tfrecords_dir):
-        dataset.to_records(
-            tfrecords_dir, test_size=test_size, parallel=parallel)
-    return dataset
-
-
-def build_nn(dataset: Dataset, config: ConfigParser) -> AtomicNN:
-    """
-    Build an `AtomicNN` with the given config and dataset.
-    """
-    section = 'nn'
-
-    l2_weight = config.getfloat(section, 'l2_weight', fallback=0.0)
-    activation = config.get(section, 'activation', fallback='leaky_relu')
-    forces = TrainableProperty.forces in dataset.trainable_properties
-
-    elements = dataset.descriptor.elements
-    hidden_sizes = {}
-    for element in elements:
-        if element in config.options(section):
-            hidden_sizes[element] = _parse_list(int,
-                                                config.get(section, element))
-        else:
-            hidden_sizes[element] = Defaults.hidden_sizes
-
-    nn = AtomicNN(elements=dataset.descriptor.elements,
-                  hidden_sizes=hidden_sizes,
-                  activation=activation,
-                  l2_weight=l2_weight,
-                  forces=forces)
-    return nn
-
-
-def get_model_hyperparams(config: ConfigParser) -> AttributeDict:
-    """
-    Parse the hyper parameters.
-    """
-    section = 'optimizer'
-
-    method = config.get(section, 'optimizer', fallback='adam')
-    learning_rate = config.getfloat(section, 'learning_rate',
-                                    fallback=Defaults.learning_rate)
-    decay_function = config.get(section, 'decay_function', fallback=None)
-    decay_rate = config.getfloat(section, 'decay_rate', fallback=None)
-    decay_steps = config.getint(section, 'decay_steps', fallback=None)
-    staircase = config.getboolean(section, 'staircase', fallback=False)
-
-    hparams = AttributeDict(method=method,
-                            learning_rate=learning_rate,
-                            decay_function=decay_function,
-                            decay_steps=decay_steps,
-                            decay_rate=decay_rate,
-                            staircase=staircase)
-
-    return hparams
 
 
 def train_and_evaluate(config: ConfigParser):
@@ -124,13 +20,13 @@ def train_and_evaluate(config: ConfigParser):
     This method is built upon `tr.estimator.train_and_evalutate`.
 
     """
-    dataset = get_dataset_from_config(config)
     graph = tf.Graph()
 
     with graph.as_default():
 
-        nn = build_nn(dataset, config)
-        hparams = get_model_hyperparams(config)
+        dataset = config.dataset
+        nn = config.nn
+        hparams = config.hyper_params
 
         section = 'train'
         model_dir = config.get(section, 'model_dir')
@@ -152,15 +48,6 @@ def train_and_evaluate(config: ConfigParser):
         tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
 
-def main(filename):
-    """
-    The main function.
-    """
-    config = ConfigParser()
-    config.read(filename)
-    train_and_evaluate(config)
-
-
 if __name__ == "__main__":
 
     parser = ArgumentParser()
@@ -169,4 +56,4 @@ if __name__ == "__main__":
         type=str,
         help="A cfg file to read."
     )
-    main(parser.parse_args().filename)
+    train_and_evaluate(ConfigParser(parser.parse_args().filename))
