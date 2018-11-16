71f1d472c1cedc25cb2d8d37666e7bf202081f41
Bismarrck <Bismarrck@me.com>
2018-11-16 16:03:56 +0800

    Add an utility function `sum_of_grads_and_vars_collections`.

--- utils.py
+++ utils.py
@@ -10,9 +10,62 @@ import logging
 from logging.config import dictConfig
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import math_ops
+from typing import Iterable
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+
+def sum_of_grads_and_vars_collections(grads_and_vars_collections):
+    """
+    Calculate the total gradient from `grad_and_vars` of different losses.
+
+    Parameters
+    ----------
+    grads_and_vars_collections: Iterable
+        A list of lists of (gradient, variable) tuples.
+
+    Returns
+    -------
+    grads_and_vars: Sized[Tuple[tf.Tensor, tf.Variable]]
+        List of pairs of (gradient, variable) as the total gradient.
+
+    """
+    # Merge gradients
+    grads_and_vars = []
+
+    for grad_and_vars in zip(*grads_and_vars_collections):
+        # Note that each grad_and_vars looks like the following:
+        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+        grads = []
+        for g, _ in grad_and_vars:
+            if g is None:
+                continue
+
+            # Add 0 dimension to the gradients to represent the tower.
+            expanded_g = tf.expand_dims(g, 0)
+
+            # Append on a 'tower' dimension which we will average over below.
+            grads.append(expanded_g)
+
+        v = grad_and_vars[0][1]
+
+        # If the grads are all None, we just return a None grad.
+        if len(grads) == 0:
+            grad_and_var = (None, v)
+
+        else:
+            # Average over the 'tower' dimension.
+            grad = tf.concat(grads, 0)
+            grad = tf.reduce_sum(grad, 0)
+
+            # Keep in mind that the Variables are redundant because they are
+            # shared across towers. So .. we will just return the first tower's
+            # pointer to the Variable.
+            grad_and_var = (grad, v)
+        grads_and_vars.append(grad_and_var)
+
+    return grads_and_vars
 
 
 def cutoff(r: tf.Tensor, rc: float, name=None):
