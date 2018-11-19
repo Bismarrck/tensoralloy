cf31ca1075c4b4d0b309a14a6ba21bbc94a09204
Bismarrck <Bismarrck@me.com>
2018-11-19 16:39:32 +0800

    Minor update.

--- utils.py
+++ utils.py
@@ -27,18 +27,18 @@ def sum_of_grads_and_vars_collections(grads_and_vars_collections):
 
     Returns
     -------
-    grads_and_vars: Sized[Tuple[tf.Tensor, tf.Variable]]
+    outputs: Sized[Tuple[tf.Tensor, tf.Variable]]
         List of pairs of (gradient, variable) as the total gradient.
 
     """
     # Merge gradients
-    grads_and_vars = []
+    outputs = []
 
-    for grad_and_vars in zip(*grads_and_vars_collections):
+    for grads_and_vars in zip(*grads_and_vars_collections):
         # Note that each grad_and_vars looks like the following:
         #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
         grads = []
-        for g, _ in grad_and_vars:
+        for g, _ in grads_and_vars:
             if g is None:
                 continue
 
@@ -48,7 +48,7 @@ def sum_of_grads_and_vars_collections(grads_and_vars_collections):
             # Append on a 'tower' dimension which we will average over below.
             grads.append(expanded_g)
 
-        v = grad_and_vars[0][1]
+        v = grads_and_vars[0][1]
 
         # If the grads are all None, we just return a None grad.
         if len(grads) == 0:
@@ -63,9 +63,9 @@ def sum_of_grads_and_vars_collections(grads_and_vars_collections):
             # shared across towers. So .. we will just return the first tower's
             # pointer to the Variable.
             grad_and_var = (grad, v)
-        grads_and_vars.append(grad_and_var)
 
-    return grads_and_vars
+        outputs.append(grad_and_var)
+    return outputs
 
 
 def cutoff(r: tf.Tensor, rc: float, name=None):
