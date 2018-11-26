dd2f3b80bf82252de551994d1c362150aad63142
Bismarrck <Bismarrck@me.com>
2018-11-26 09:35:54 +0800

    Move `get_conversion_units` to this module and add a test for it.

---
+++ tensoralloy/units.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+"""
+This module defines the unit conversion function.
+"""
+from __future__ import print_function, absolute_import
+
+import re
+import ase.units
+from typing import Dict
+
+__author__ = 'Xin Chen'
+__email__ = 'Bismarrck@me.com'
+
+
+def get_conversion_units(units: Dict[str, str]) -> (float, float, float):
+    """
+    Return the conversion factors:
+        * 'energy' should be converted to 'eV'
+        * 'forces' should be converted to 'eV / Angstrom'
+        * 'stress' should be converted to 'GPa'
+
+    """
+    _units = {
+        'kbar': 0.1 * ase.units.GPa,
+    }
+
+    def _parse_unit(unit: str):
+        if hasattr(ase.units, unit):
+            return getattr(ase.units, unit)
+        elif unit in _units:
+            return _units[unit]
+        else:
+            try:
+                return float(unit)
+            except Exception:
+                raise ValueError("Unknown unit: {}".format(unit))
+
+    def _parse_comb(comb: str):
+        if '/' in comb or '*' in comb:
+            values = [_parse_unit(unit) for unit in re.split(r'[/*]', comb)]
+            total = values[0]
+            index = 1
+            for i in range(len(comb)):
+                if comb[i] == '/':
+                    total /= values[index]
+                    index += 1
+                elif comb[i] == '*':
+                    total *= values[index]
+                    index += 1
+                if index == len(values):
+                    break
+            return total
+        else:
+            return getattr(ase.units, comb)
+
+    eV = ase.units.eV
+    Angstrom = ase.units.Angstrom
+    GPa = ase.units.GPa
+
+    if 'energy' not in units:
+        to_eV = 1.0
+    else:
+        to_eV = _parse_comb(units['energy']) / eV
+
+    if 'forces' not in units:
+        to_eV_Angstrom = 1.0
+    else:
+        to_eV_Angstrom = _parse_comb(units['forces']) / eV / Angstrom
+
+    if 'stress' not in units:
+        to_GPa = 1.0
+    else:
+        to_GPa = _parse_comb(units['stress']) / GPa
+
+    return to_eV, to_eV_Angstrom, to_GPa
