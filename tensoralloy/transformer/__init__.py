09a22bbe22c9a19ac4288a98955432f08ac672f7
Bismarrck <Bismarrck@me.com>
2018-11-26 15:33:28 +0800

    Minor update.

--- tensoralloy/transformer/__init__.py
+++ tensoralloy/transformer/__init__.py
@@ -1,4 +1,16 @@
+# coding=utf-8
+"""
+The module `tensoralloy.transformer` is used to convert `Atoms` objects to
+features.
+"""
 from __future__ import print_function, absolute_import
+
+from .behler import BatchSymmetryFunctionTransformer
+from .behler import SymmetryFunctionTransformer
+from .eam import BatchEAMTransformer
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+__all__ = ["BatchSymmetryFunctionTransformer", "BatchEAMTransformer",
+           "SymmetryFunctionTransformer"]
