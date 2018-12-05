7cb91c4df1720ba9b39a2fe475ad306eec095475
Bismarrck <Bismarrck@me.com>
2018-12-05 15:40:01 +0800

    The tests passed.

--- tensoralloy/nn/eam/potentials/tests/test_potentials.py
+++ tensoralloy/nn/eam/potentials/tests/test_potentials.py
@@ -7,20 +7,20 @@ from __future__ import print_function, absolute_import
 import nose
 from nose.tools import assert_list_equal, assert_dict_equal
 
-from ..potentials import EmpiricalPotential, any_kbody_term
+from ..potentials import EmpiricalPotential
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
 
 
 # noinspection PyAbstractClass
-class _ParamLayer(EmpiricalPotential):
+class _ParamPotential(EmpiricalPotential):
     """
     A layer with default parameters 'a', 'b', 'c' and 'd'.
     """
 
     defaults = {
-        any_kbody_term: {'a': 1.0, 'b': 1.0, 'c': 1.0, 'd': 1.0}
+        'AlCu': {'a': 1.0, 'b': 1.0, 'c': 1.0, 'd': 1.0}
     }
 
 
@@ -30,26 +30,22 @@ def test_layer_init():
     """
     layer = EmpiricalPotential()
 
-    assert_list_equal(layer.allowed_kbody_terms, [any_kbody_term])
-    assert_dict_equal(layer.params, {any_kbody_term: {}})
+    assert_dict_equal(layer.params, {})
     assert_dict_equal(layer.fixed, {})
 
     layer = EmpiricalPotential(
         params={'AlCu': {"x": 2, "y": 3}}, fixed={'AlCu': ['x']})
 
-    assert_dict_equal(layer.params, {any_kbody_term: {},
-                                     'AlCu': {"x": 2, "y": 3}})
+    assert_dict_equal(layer.params, {'AlCu': {"x": 2, "y": 3}})
     assert_dict_equal(layer.fixed, {'AlCu': ['x']})
 
-    layer = _ParamLayer()
-    assert_dict_equal(layer.params, {any_kbody_term: {'a': 1.0, 'b': 1.0,
-                                                      'c': 1.0, 'd': 1.0}})
+    layer = _ParamPotential()
+    assert_dict_equal(layer.params, {'AlCu': {'a': 1.0, 'b': 1.0,
+                                              'c': 1.0, 'd': 1.0}})
 
-    layer = _ParamLayer(params={'AlCu': {"a": 2.0, "b": 3.0}})
+    layer = _ParamPotential(params={'AlCu': {"a": 2.0, "b": 3.0}})
     assert_dict_equal(layer.params,
-                      {'AlCu': {'a': 2.0, 'b': 3.0, 'c': 1.0, 'd': 1.0},
-                       any_kbody_term: {'a': 1.0, 'b': 1.0,
-                                        'c': 1.0, 'd': 1.0}})
+                      {'AlCu': {'a': 2.0, 'b': 3.0, 'c': 1.0, 'd': 1.0}})
 
 
 if __name__ == "__main__":
