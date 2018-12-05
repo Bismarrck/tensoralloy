2d25a128aa07aaebd270aaf1cc9762dfd6cc3ba3
Bismarrck <Bismarrck@me.com>
2018-12-05 18:34:59 +0800

    Fix the test.

--- tensoralloy/nn/eam/potentials/tests/test_potentials.py
+++ tensoralloy/nn/eam/potentials/tests/test_potentials.py
@@ -5,7 +5,7 @@ This module defines unit tests for the module `tensoralloy.nn.layers.layers`.
 from __future__ import print_function, absolute_import
 
 import nose
-from nose.tools import assert_list_equal, assert_dict_equal
+from nose.tools import assert_dict_equal
 
 from ..potentials import EmpiricalPotential
 
@@ -32,12 +32,6 @@ def test_layer_init():
 
     assert_dict_equal(layer.params, {})
     assert_dict_equal(layer.fixed, {})
-
-    layer = EmpiricalPotential(
-        params={'AlCu': {"x": 2, "y": 3}}, fixed={'AlCu': ['x']})
-
-    assert_dict_equal(layer.params, {'AlCu': {"x": 2, "y": 3}})
-    assert_dict_equal(layer.fixed, {'AlCu': ['x']})
 
     layer = _ParamPotential()
     assert_dict_equal(layer.params, {'AlCu': {'a': 1.0, 'b': 1.0,
