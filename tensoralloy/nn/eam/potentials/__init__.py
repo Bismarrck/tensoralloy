59572848b0251ceffa4c6347d37396844a0e74a3
Bismarrck <Bismarrck@me.com>
2018-12-06 16:43:30 +0800

    Add `AlFeMsah11` to `availabel_potentials`.

--- tensoralloy/nn/eam/potentials/__init__.py
+++ tensoralloy/nn/eam/potentials/__init__.py
@@ -4,6 +4,7 @@ from __future__ import print_function, absolute_import
 from .potentials import EamAlloyPotential, EamFSPotential
 from .sutton90 import AgSutton90
 from .zjw04 import AlCuZJW04
+from .msah11 import AlFeMsah11
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
@@ -11,5 +12,6 @@ __email__ = 'Bismarrck@me.com'
 
 available_potentials = {
     'sutton90': AgSutton90,
-    'zjw04': AlCuZJW04
+    'zjw04': AlCuZJW04,
+    'msah11': AlFeMsah11,
 }
