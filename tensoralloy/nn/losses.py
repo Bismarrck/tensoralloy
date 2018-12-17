734d58b547884df8c3ca0516a3e5adead46f56a9
Bismarrck <Bismarrck@me.com>
2018-12-17 16:05:57 +0800

    The loss functions accept `compositions` as an optional argument so that zero-padding will not affect calculation results.

--- tensoralloy/nn/losses.py
+++ tensoralloy/nn/losses.py
@@ -11,11 +11,29 @@ __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
 
 
-def rmse_loss(labels, predictions, name=None, add_eps=False):
+def _get_weight(compositions, n_max, dtype=tf.float64, name=None):
+    """
+    Compute the scaling factor.
+    """
+    with ops.name_scope(name, "Scale", [compositions, n_max]):
+        compositions = tf.convert_to_tensor(
+            compositions, name='compositions')
+        if compositions.shape.ndims != 2:
+            raise ValueError("The ndims of `compositions` should be 2")
+        n_reals = tf.reduce_sum(
+            compositions, axis=1, keepdims=False, name='n_reals')
+        if n_reals.dtype != dtype:
+            n_reals = tf.cast(n_reals, dtype=dtype)
+        n_max = tf.convert_to_tensor(n_max, dtype=dtype, name='n_max')
+        one = tf.constant(1.0, dtype=tf.float64, name='one')
+        return tf.div(one, tf.reduce_mean(n_reals / n_max), name='weight')
+
+
+def rmse_loss(labels, predictions, compositions=None, name=None, add_eps=False):
     """
     Return the root mean squared error as the loss.
     """
-    with ops.name_scope(name, "rmse", [labels, predictions]) as name:
+    with ops.name_scope(name, "RmseLoss", [labels, predictions]) as name:
         x = tf.convert_to_tensor(labels, name='x')
         y = tf.convert_to_tensor(predictions, name='y')
         mse = tf.reduce_mean(tf.squared_difference(x, y), name='mse')
@@ -26,31 +44,62 @@ def rmse_loss(labels, predictions, name=None, add_eps=False):
             with tf.name_scope("safe_sqrt"):
                 eps = tf.constant(1e-14, dtype=tf.float64, name='eps')
                 mse = tf.add(mse, eps)
+        if compositions is not None:
+            assert x.shape.ndims >= 2
+            weight = _get_weight(compositions, x.shape[1].value, x.dtype)
+            mse = tf.multiply(weight, mse, name='scaled')
         return tf.sqrt(mse, name=name)
 
 
-def norm_loss(labels, predictions, name=None):
+def norm_loss(labels, predictions, compositions=None, name=None):
     """
     Return the ratio of 2-norms of `labels - predictions` and `labels` as the
     loss.
     """
-    with ops.name_scope(name, "norm", [labels, predictions]) as name:
+    with ops.name_scope(
+            name, "NormLoss", [labels, predictions, compositions]) as name:
         x = tf.convert_to_tensor(labels, name='x')
         y = tf.convert_to_tensor(predictions, name='y')
-        diff = tf.subtract(x, y)
         ndims = x.shape.ndims
         axis = ndims - 1
-        upper = tf.linalg.norm(
-            diff, ord=2, axis=axis, keepdims=False, name='l2u')
-        lower = tf.linalg.norm(x, ord=2, axis=axis, keepdims=False, name='l2l')
-        return tf.reduce_mean(tf.div_no_nan(upper, lower), name=name)
+        diff = tf.subtract(x, y)
+
+        with ops.name_scope("l2"):
+            upper = tf.linalg.norm(
+                diff, ord=2, axis=axis, keepdims=False, name='upper')
+            lower = tf.linalg.norm(
+                x, ord=2, axis=axis, keepdims=False, name='lower')
+
+        with ops.name_scope("safe_div"):
+            threshold = tf.constant(1e-2, name='threshold', dtype=x.dtype)
+            idx0 = tf.where(tf.less(lower, threshold), name='idx0')
+            idx1 = tf.where(tf.greater_equal(lower, threshold), name='idx1')
+            shape = lower.shape
+            ratio = tf.div_no_nan(upper, lower)
+            norm = tf.scatter_nd(
+                idx0, tf.gather_nd(upper, idx0), shape, name='norm')
+            ratio = tf.scatter_nd(
+                idx1, tf.gather_nd(ratio, idx1), shape, name='ratio')
+            values = ratio + norm
+
+        if compositions is not None:
+            assert ndims >= 2
+            weight = _get_weight(compositions, x.shape[1].value, x.dtype)
+            values = tf.multiply(values, weight, name='scaled')
+
+        return tf.reduce_mean(values, name=name)
 
 
-def mae_loss(labels, predictions, name=None):
+def mae_loss(labels, predictions, compositions=None, name=None):
     """
     Return the mean absolute error as the loss.
     """
-    with ops.name_scope(name, "mae", [labels, predictions]) as name:
+    with ops.name_scope(name, "MaeLoss", [labels, predictions]) as name:
         x = tf.convert_to_tensor(labels, name='x')
         y = tf.convert_to_tensor(predictions, name='y')
-        return tf.reduce_mean(tf.abs(x - y), name=name)
+        mae = tf.reduce_mean(tf.abs(x - y))
+        if compositions is not None:
+            assert x.shape.ndims >= 2
+            weight = _get_weight(compositions, x.shape[1].value, x.dtype)
+            mae = tf.multiply(weight, mae, name='scaled')
+        return tf.identity(mae, name=name)
--- tensoralloy/nn/tests/test_losses.py
+++ tensoralloy/nn/tests/test_losses.py
@@ -20,45 +20,76 @@ def test_norm_loss():
     """
     Test the function `norm_loss`.
     """
+    compositions = np.asarray([[5], [6], [4], [8], [8], [5]])
     x = np.random.rand(6, 8, 3)
-    x[5, 7] = 0.0
     y = np.random.rand(6, 8, 3)
+
+    for i in range(len(x)):
+        x[i, compositions[i, 0]:] = 0.0
+        y[i, compositions[i, 0]:] = 0.0
+    x[5] = 0.0
 
     values = []
     for i in range(6):
-        for j in range(8):
+        for j in range(compositions[i, 0]):
             a = np.linalg.norm(x[i, j])
-            if a == 0.0:
-                values.append(0.0)
+            if a < 0.01:
+                values.append(np.linalg.norm(y[i, j]))
             else:
                 b = np.linalg.norm(x[i, j] - y[i, j])
                 values.append(b / a)
     ref = np.mean(values)
 
     with tf.Graph().as_default():
-        loss = norm_loss(x, y, name='loss')
+        loss = norm_loss(x, y, compositions, name='loss')
         with tf.Session() as sess:
-            assert_less(ref - sess.run(loss), 1e-8)
+            pred = sess.run(loss)
+            assert_less(ref - pred, 1e-8)
 
 
 def test_rmse_and_mae_losses():
     """
-    Test the functions `rmse_loss` and `mae_loss`.
+    Test the functions `rmse_loss` and `mae_loss` with `compositions`.
     """
-    x = np.random.rand(6, 8, 3)
+    x = np.random.uniform(0.0, 3.0, size=(6, 8, 3))
     x[5] = 0.0
-    y = np.random.rand(6, 8, 3)
+    y = np.random.uniform(0.0, 3.0, size=(6, 8, 3))
     y[5] = 0.0
+    compositions = np.asarray([[5], [6], [4], [8], [5], [3]])
 
     mae_values = []
     rmse_values = []
 
     for i in range(len(x)):
-        mae_values.append(mean_absolute_error(x[i], y[i]))
-        rmse_values.append(mean_squared_error(x[i], y[i]))
+        n = compositions[i, 0]
+        for j in range(n):
+            mae_values.append(np.abs(x[i, j] - y[i, j]))
+            rmse_values.append(np.square(x[i, j] - y[i, j]))
 
     y_mae = np.mean(mae_values)
-    y_rmse = np.mean(rmse_values)
+    y_rmse = np.sqrt(np.mean(rmse_values))
+
+    with tf.Graph().as_default():
+
+        mae = mae_loss(x, y, compositions=compositions, name='mae')
+        rmse = rmse_loss(x, y, compositions=compositions, name='rmse')
+
+        with tf.Session() as sess:
+            assert_less(y_mae - sess.run(mae), 1e-8)
+            assert_less(y_rmse - sess.run(rmse), 1e-8)
+
+
+def test_normal_rmse_and_mae_losses():
+    """
+    Test the functions `rmse_loss` and `mae_loss` without `compositions`.
+    """
+    x = np.random.uniform(0.0, 3.0, size=(6, 8))
+    x[5] = 0.0
+    y = np.random.uniform(0.0, 3.0, size=(6, 8))
+    y[5] = 0.0
+
+    y_mae = mean_absolute_error(x, y)
+    y_rmse = np.sqrt(mean_squared_error(x, y))
 
     with tf.Graph().as_default():
 
