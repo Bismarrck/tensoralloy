cbf299fb26002c4c533cb45a37bcc9c3beb22b67
Bismarrck <Bismarrck@me.com>
2018-12-20 15:11:50 +0800

    A strange bug shall be addressed: this module cannot be cythonized as a TypeError should raise.

--- tensoralloy/descriptor/cutoff.py
+++ tensoralloy/descriptor/cutoff.py
@@ -26,9 +26,13 @@ def cosine_cutoff(r: tf.Tensor, rc: float, name=None):
     PRL 98, 146401 (2007)
 
     """
-    with ops.name_scope(name, "fc", [r]) as name:
-        rc = ops.convert_to_tensor(rc, dtype=r.dtype, name="rc")
+    with ops.name_scope(name, "CosCutoff", [r]) as name:
+        # TODO: 'TypeError: must be real number, not Tensor' will occur here if
+        #  this module was cythonized.
+        rc = ops.convert_to_tensor(rc, dtype=tf.float64, name="rc")
         ratio = math_ops.div(r, rc, name='ratio')
-        z = math_ops.minimum(ratio, 1.0, name='minimum')
-        z = math_ops.cos(z * np.pi, name='cos') + 1.0
-        return math_ops.multiply(z, 0.5, name=name)
+        one = ops.convert_to_tensor(1.0, dtype=tf.float64, name='one')
+        half = ops.convert_to_tensor(0.5, dtype=tf.float64, name='half')
+        z = math_ops.minimum(ratio, one, name='minimum')
+        z = math_ops.cos(z * np.pi, name='cos') + one
+        return math_ops.multiply(z, half, name=name)
