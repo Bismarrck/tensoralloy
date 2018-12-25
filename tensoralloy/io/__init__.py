5934f40c51c19a0905e64eb7845ac8a537ef1df4
Bismarrck <Bismarrck@me.com>
2018-12-25 14:50:47 +0800

    Minor update.

--- tensoralloy/io/__init__.py
+++ tensoralloy/io/__init__.py
@@ -1,4 +1,13 @@
+# coding=utf-8
+"""
+The `tensoralloy.io` module.
+"""
 from __future__ import print_function, absolute_import
+
+from .input.reader import InputReader
+from .read import read
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+__all__ = ["InputReader", "read"]
