132f6ca9b36d48e09e2a568919daed2642ec805d
Bismarrck <Bismarrck@me.com>
2019-01-09 13:01:45 +0800

    Add a new sutton90/eam test using `EAMTransformer`.

--- tensoralloy/nn/eam/potentials/tests/test_sutton90.py
+++ tensoralloy/nn/eam/potentials/tests/test_sutton90.py
@@ -7,6 +7,7 @@ from __future__ import print_function, absolute_import
 import tensorflow as tf
 import nose
 import os
+
 from ase.build import bulk
 from ase.calculators.lammpsrun import LAMMPS
 from collections import Counter
@@ -16,7 +17,7 @@ from unittest import skipUnless
 from nose.tools import assert_almost_equal
 from nose import with_setup
 
-from tensoralloy.transformer import BatchEAMTransformer
+from tensoralloy.transformer import BatchEAMTransformer, EAMTransformer
 from tensoralloy.nn.eam.alloy import EamAlloyNN
 from tensoralloy.io.neighbor import find_neighbor_sizes
 from tensoralloy.misc import AttributeDict, test_dir
@@ -33,6 +34,9 @@ else:
     LAMMPS_COMMAND = os.environ['LAMMPS_COMMAND']
 
 
+tmp_dir = join(test_dir(absolute=True), 'lammps', 'sutton90')
+
+
 def get_lammps_calculator():
     """
     Return a LAMMPS calculator for Ag.
@@ -40,33 +44,31 @@ def get_lammps_calculator():
     eam_file = join(test_dir(absolute=True), 'lammps', 'Ag.funcfl.eam')
     parameters = {'pair_style': 'eam',
                   'pair_coeff': ['1 1 Ag.funcfl.eam']}
-    work_dir = join(test_dir(absolute=True), 'lammps', 'tmp')
-    return LAMMPS(files=[eam_file], parameters=parameters, tmp_dir=work_dir,
-                  keep_tmp_files=False, keep_alive=False, no_data_file=True)
-
-
-lammps = get_lammps_calculator()
+    return LAMMPS(files=[eam_file], parameters=parameters, tmp_dir=tmp_dir,
+                  keep_tmp_files=False, keep_alive=False, no_data_file=False)
 
 
 def teardown():
     """
     Delete the tmp dir.
     """
-    if exists(lammps.tmp_dir):
-        rmtree(lammps.tmp_dir, ignore_errors=True)
+    if exists(tmp_dir):
+        rmtree(tmp_dir, ignore_errors=True)
 
 
 @with_setup(teardown=teardown)
-@skipUnless(exists(LAMMPS_COMMAND), f"{LAMMPS_COMMAND} not exists!")
-def test_eam_sutton90():
+@skipUnless(exists(LAMMPS_COMMAND), f"{LAMMPS_COMMAND} not set!")
+def test_eam_sutton90_batch_transformer():
     """
-    Test the total energy calculation of `EamNN` with `AgSutton90`.
+    Test the total energy calculation of `EamAlloyNN` with `AgSutton90` using
+    `BatchEAMTransformer`
     """
     ref = bulk('Ag') * [2, 2, 2]
     rc = 11.999
     nij_max, _, nnl_max = find_neighbor_sizes(ref, rc=rc, k_max=2)
     max_occurs = Counter(ref.get_chemical_symbols())
 
+    lammps = get_lammps_calculator()
     atoms = bulk('Ag') * [2, 2, 1]
     atoms.calc = lammps
 
@@ -95,7 +97,9 @@ def test_eam_sutton90():
         nn = EamAlloyNN(elements=['Ag'], custom_potentials={
             "Ag": {"rho": "sutton90", "embed": "sutton90"},
             "AgAg": {"phi": "sutton90"}})
-        prediction = nn.build(features, verbose=False)
+        prediction = nn.build(features=features,
+                              mode=tf.estimator.ModeKeys.EVAL,
+                              verbose=False)
 
         with tf.Session() as sess:
             tf.global_variables_initializer().run()
@@ -103,6 +107,41 @@ def test_eam_sutton90():
 
         assert_almost_equal(energy,
                             lammps.get_potential_energy(atoms), delta=1e-6)
+
+
+@with_setup(teardown=teardown)
+@skipUnless(exists(LAMMPS_COMMAND), f"{LAMMPS_COMMAND} not set!")
+def test_eam_sutton90():
+    """
+    Test the total energy calculation of `EamAlloyNN` with `AgSutton90` using
+    `EAMTransformer`
+    """
+    rc = 11.999
+    lammps = get_lammps_calculator()
+
+    atoms = bulk('Ag') * [2, 2, 2]
+    atoms.calc = lammps
+    elements = ['Ag']
+
+    with tf.Graph().as_default():
+        clf = EAMTransformer(rc=rc, elements=elements)
+        nn = EamAlloyNN(elements=elements,
+                        custom_potentials={
+                            "Ag": {"rho": "sutton90", "embed": "sutton90"},
+                            "AgAg": {"phi": "sutton90"}})
+        prediction = nn.build(
+            features=clf.get_features(),
+            mode=tf.estimator.ModeKeys.PREDICT,
+            verbose=True)
+
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            energy = float(sess.run(prediction.energy,
+                                    feed_dict=clf.get_feed_dict(atoms)))
+
+    atoms.calc = lammps
+    assert_almost_equal(energy,
+                        lammps.get_potential_energy(atoms), delta=1e-6)
 
 
 if __name__ == "__main__":
