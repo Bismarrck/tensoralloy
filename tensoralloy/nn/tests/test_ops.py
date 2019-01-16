59a88ac93aa007334e7a9df675f03f9ef9f8df98
Bismarrck <Bismarrck@me.com>
2019-01-16 13:50:57 +0800

    Also test `get_train_op` with trainable symmetry function parameters.

--- tensoralloy/nn/tests/test_ops.py
+++ tensoralloy/nn/tests/test_ops.py
@@ -21,42 +21,63 @@ __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
 
 
+def _get_train_op(trainable=False):
+    """
+    A helper function to created a `train_op`.
+    """
+    work_dir = join(test_dir(), 'Ni')
+    dataset = Dataset(connect(join(work_dir, 'Ni.db')), name='Ni',
+                      descriptor='behler', serial=True, trainable=trainable)
+
+    if not dataset.load_tfrecords(work_dir):
+        dataset.to_records(work_dir, test_size=1)
+    assert_equal(dataset.test_size, 1)
+
+    input_fn = dataset.input_fn(mode=tf.estimator.ModeKeys.TRAIN,
+                                batch_size=1,
+                                num_epochs=None,
+                                shuffle=False)
+
+    features, labels = input_fn()
+
+    hparams = AttributeDict(
+        loss=AttributeDict(
+            energy=AttributeDict(weight=1.0)),
+        opt=AttributeDict(
+            learning_rate=0.01, decay_function=None, decay_steps=1000,
+            decay_rate=0.9, staircase=False, method='adam'))
+
+    nn = AtomicNN(elements=dataset.transformer.elements)
+    predictions = nn.build(features, tf.estimator.ModeKeys.TRAIN)
+    total_loss, losses = nn.get_total_loss(
+        predictions, labels, features.n_atoms, hparams)
+
+    return get_train_op(losses, hparams, nn.minimize_properties)
+
+
 def test_get_train_op():
     """
     Test the function `get_train_op`.
     """
-    with tf.Graph().as_default():
-        work_dir = join(test_dir(), 'Ni')
-        dataset = Dataset(connect(join(work_dir, 'Ni.db')), name='Ni',
-                          descriptor='behler', serial=True)
+    tf.reset_default_graph()
 
-        if not dataset.load_tfrecords(work_dir):
-            dataset.to_records(work_dir, test_size=1)
-        assert_equal(dataset.test_size, 1)
+    graph = tf.Graph()
 
-        input_fn = dataset.input_fn(mode=tf.estimator.ModeKeys.TRAIN,
-                                    batch_size=1,
-                                    num_epochs=None,
-                                    shuffle=False)
+    with graph.as_default():
 
-        features, labels = input_fn()
-
-        hparams = AttributeDict(
-            loss=AttributeDict(
-                energy=AttributeDict(weight=1.0)),
-            opt=AttributeDict(
-                learning_rate=0.01, decay_function=None, decay_steps=1000,
-                decay_rate=0.9, staircase=False, method='adam'))
-
-        nn = AtomicNN(elements=dataset.transformer.elements)
-        predictions = nn.build(features, tf.estimator.ModeKeys.TRAIN)
-        total_loss, losses = nn.get_total_loss(
-            predictions, labels, features.n_atoms, hparams)
-
-        get_train_op(losses, hparams, nn.minimize_properties)
+        _get_train_op(trainable=False)
 
         assert_equal(len(tf.trainable_variables()), 5)
         assert_equal(len(tf.moving_average_variables()), 5)
+        assert_equal(len(tf.model_variables()), 9)
+
+    graph = tf.Graph()
+
+    with graph.as_default():
+        _get_train_op(trainable=True)
+
+        assert_equal(len(tf.trainable_variables()), 9)
+        assert_equal(len(tf.moving_average_variables()), 9)
         assert_equal(len(tf.model_variables()), 9)
 
 
