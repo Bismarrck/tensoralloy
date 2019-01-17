08fa49cf5203aeef4fdf1cf80e5c14e0ff3394ca
Bismarrck <Bismarrck@me.com>
2019-01-17 16:18:03 +0800

    Fix the test `test_calculator_with_qm7`.

--- tensoralloy/tests/test_calculator.py
+++ tensoralloy/tests/test_calculator.py
@@ -15,43 +15,35 @@ from sklearn.model_selection import train_test_split
 
 from tensoralloy.calculator import TensorAlloyCalculator
 from tensoralloy.utils import Defaults
-from tensoralloy.test_utils import test_dir
+from tensoralloy.test_utils import test_dir, datasets_dir
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
 
 
-def test_calculator_with_nickel():
+def test_calculator_with_qm7():
     """
-    Test `TensorAlloyCalculator` using the Ni database.
+    Test total energy calculation of `TensorAlloyCalculator` using the QM7
+    dataset.
     """
-    graph_path = join(test_dir(), 'checkpoints', 'Ni-k2', 'Ni.pb')
-    sqlite3_file = join(test_dir(), 'checkpoints', 'Ni-k2', 'Ni.db')
+    graph_path = join(test_dir(), 'checkpoints', 'qm7-k2', 'qm7.pb')
+    sqlite3_file = join(datasets_dir(), 'qm7.db')
 
-    def run_test():
-        """ A wrapper. """
-        calc = TensorAlloyCalculator(graph_path)
+    calc = TensorAlloyCalculator(graph_path)
 
-        db = connect(sqlite3_file)
-        _, tests = train_test_split(np.arange(1, 1 + len(db)),
-                                    random_state=Defaults.seed, test_size=100)
+    db = connect(sqlite3_file)
+    _, tests = train_test_split(np.arange(1, 1 + len(db)),
+                                random_state=Defaults.seed, test_size=1000)
 
-        y_true = []
-        y_pred = []
-        for index in tests:
-            atoms = db.get_atoms(f'id={index}')
-            y_true.append(atoms.get_total_energy())
-            y_pred.append(calc.get_potential_energy(atoms))
+    y_true = []
+    y_pred = []
+    for index in tests:
+        atoms = db.get_atoms(f'id={index}')
+        y_true.append(atoms.get_total_energy())
+        y_pred.append(calc.get_potential_energy(atoms))
 
-        y_mae = mean_absolute_error(y_true, y_pred)
-        assert_almost_equal(y_mae, 0.00769633, delta=1e-7)
-
-        calc.get_forces(db.get_atoms('id=1'))
-
-    # The model file should be updated because the arg `k_max` is replaced with
-    # `angular`.
-    with assert_raises(Exception):
-        run_test()
+    y_mae = mean_absolute_error(y_true, y_pred)
+    assert_almost_equal(y_mae, 0.12429088, delta=1e-7)
 
 
 if __name__ == "__main__":
