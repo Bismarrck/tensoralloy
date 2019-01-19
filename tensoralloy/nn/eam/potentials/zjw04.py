cf09db0fcffb7974621db5d4e4661506fd59d204
Xin Chen <Bismarrck@me.com>
2019-01-19 09:44:51 +0800

    Fix a bug.

--- tensoralloy/nn/eam/potentials/zjw04.py
+++ tensoralloy/nn/eam/potentials/zjw04.py
@@ -261,8 +261,10 @@ class Zjw04(EamAlloyPotential):
             rho_e = self._get_shared_variable('rho_e', dtype, element)
             rho_s = self._get_shared_variable('rho_s', dtype, element)
             Fe = self._get_shared_variable('Fe', dtype, element)
-            rho_n = tf.multiply(0.85, rho_e, name='rho_0')
-            rho_0 = tf.multiply(1.15, rho_e, name='rho_0')
+            rho_n = tf.multiply(tf.constant(0.85, dtype=dtype, name='lb'),
+                                rho_e, name='rho_0')
+            rho_0 = tf.multiply(tf.constant(1.15, dtype=dtype, name='ub'),
+                                rho_e, name='rho_0')
             one = tf.constant(1.0, dtype, name='one')
 
             def embed1(_rho):
