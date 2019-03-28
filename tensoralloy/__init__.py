5a7fcb18645d048432bfca3b7773195a89fd2128
Bismarrck <Bismarrck@me.com>
2019-03-28 10:25:00 +0800

    Minor update.

--- tensoralloy/__init__.py
+++ tensoralloy/__init__.py
@@ -1,4 +1,12 @@
+#!coding=utf-8
+"""
+Core modules will be imported.
+"""
 from __future__ import print_function, absolute_import
+
+from tensoralloy.calculator import TensorAlloyCalculator
+from tensoralloy.io.input import InputReader
+from tensoralloy.train import TrainingManager
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
