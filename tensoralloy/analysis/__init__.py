9b5fd43410c3aa51fa73b9ede8a5aaaec80b284e
Bismarrck <Bismarrck@me.com>
2019-04-08 18:49:42 +0800

    Move module `phonopy` to this new subpackage `tensoralloy.analysis`.

---
+++ tensoralloy/analysis/__init__.py
@@ -0,0 +1,8 @@
+#!coding=utf-8
+"""
+This subpackage contains routines for analyzing material properties.
+"""
+from __future__ import print_function, absolute_import
+
+__author__ = 'Xin Chen'
+__email__ = 'Bismarrck@me.com'
