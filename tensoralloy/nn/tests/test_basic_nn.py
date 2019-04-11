ff226994b6e4df0f8746d816032f98336a13b512
Bismarrck <Bismarrck@me.com>
2019-04-11 16:58:32 +0800

    Add a test for inference with different properties.

--- tensoralloy/nn/tests/test_basic_nn.py
+++ tensoralloy/nn/tests/test_basic_nn.py
@@ -8,15 +8,22 @@ import tensorflow as tf
 import numpy as np
 import nose
 
-from nose.tools import assert_dict_equal, assert_equal
+from tensorflow_estimator import estimator as tf_estimator
+from nose.tools import assert_dict_equal, assert_equal, assert_true
 from ase.db import connect
 from os.path import join
+from collections import Counter
+from typing import List
 from tensorflow.core.framework import graph_pb2
 from tensorflow.python.framework import importer
 
+from tensoralloy.io.neighbor import find_neighbor_size_of_atoms
 from tensoralloy.nn.basic import BasicNN
+from tensoralloy.nn.atomic import AtomicNN
+from tensoralloy.transformer import BatchSymmetryFunctionTransformer
 from tensoralloy.utils import AttributeDict, Defaults
 from tensoralloy.test_utils import assert_array_equal, datasets_dir, test_dir
+from tensoralloy.dtypes import set_float_precision
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
@@ -155,6 +162,88 @@ def test_graph_model_variables():
                 value = sess.run(f"{var_op_name}:0")
                 dmax = np.abs(np.atleast_1d(value - ref_value)).max()
                 assert_equal(dmax, 0)
+
+
+def test_build_nn_with_properties():
+    """
+    Test the method `BasicNN.build` with different `minimize_properties`.
+    """
+    elements = ['Mo', 'Ni']
+    rc = 6.5
+    batch_size = 1
+    db = connect(join(datasets_dir(), 'snap.db'))
+    atoms = db.get_atoms(id=1)
+    nij_max, _, _ = find_neighbor_size_of_atoms(atoms, rc, k_max=2)
+    max_occurs = Counter(atoms.get_chemical_symbols())
+
+    def _get_transformer():
+        """
+        A helper function to return a `BatchSymmetryFunctionTransformer`.
+        """
+        return BatchSymmetryFunctionTransformer(
+            rc=rc, max_occurs=max_occurs, nij_max=nij_max, nijk_max=0,
+            batch_size=batch_size, use_stress=True, use_forces=True)
+
+    def _test_with_properties(list_of_properties: List[str]):
+        """
+        Run a test.
+        """
+        with tf.Graph().as_default():
+            nn = AtomicNN(elements, minimize_properties=list_of_properties)
+            clf = _get_transformer()
+            nn.attach_transformer(clf)
+            protobuf = tf.convert_to_tensor(
+                clf.encode(atoms).SerializeToString())
+            example = clf.decode_protobuf(protobuf)
+            batch = AttributeDict()
+            for key, tensor in example.items():
+                batch[key] = tf.expand_dims(
+                    tensor, axis=0, name=tensor.op.name + '/batch')
+            labels = AttributeDict(energy=batch.pop('y_true'),
+                                   energy_confidence=batch.pop('y_conf'))
+            labels['forces'] = batch.pop('f_true')
+            labels['forces_confidence'] = batch.pop('f_conf')
+            labels['stress'] = batch.pop('stress')
+            labels['stress_confidence'] = batch.pop('s_conf')
+            labels['total_pressure'] = batch.pop('total_pressure')
+
+            hparams = AttributeDict()
+            hparams.loss = AttributeDict(
+                equivalently_trusted=True,
+                energy=AttributeDict(weight=1.0, per_atom_loss=False),
+                forces=AttributeDict(weight=1.0),
+                stress=AttributeDict(weight=1.0, use_rmse=True),
+                total_pressure=AttributeDict(weight=1.0),
+                l2=AttributeDict(weight=0.01),
+                elastic=AttributeDict(weight=0.1, crystals=['Ni'],
+                                      constraint_weight=10.0),
+            )
+            mode = tf_estimator.ModeKeys.TRAIN
+
+            try:
+                predictions = nn.build(batch, mode=mode, verbose=True)
+            except Exception:
+                return False
+            try:
+                nn.get_total_loss(predictions=predictions,
+                                  labels=labels,
+                                  n_atoms=batch.n_atoms,
+                                  hparams=hparams,
+                                  mode=mode)
+            except Exception as excp:
+                print(excp)
+                return False
+            else:
+                return True
+
+    set_float_precision('medium')
+
+    for case in (['energy', 'elastic'],
+                 ['energy', 'stress'],
+                 ['energy', 'elastic']):
+        assert_true(_test_with_properties(case), msg=f"{case} is failed")
+
+    set_float_precision('high')
 
 
 if __name__ == "__main__":
