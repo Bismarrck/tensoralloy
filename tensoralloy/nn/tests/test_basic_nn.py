8029f350a78c702532d3cd99baba803d650d616b
Bismarrck <Bismarrck@me.com>
2019-04-11 12:45:04 +0800

    Add a new unit test: `test_graph_model_variables`.

--- tensoralloy/nn/tests/test_basic_nn.py
+++ tensoralloy/nn/tests/test_basic_nn.py
@@ -11,10 +11,12 @@ import nose
 from nose.tools import assert_dict_equal, assert_equal
 from ase.db import connect
 from os.path import join
+from tensorflow.core.framework import graph_pb2
+from tensorflow.python.framework import importer
 
 from tensoralloy.nn.basic import BasicNN
 from tensoralloy.utils import AttributeDict, Defaults
-from tensoralloy.test_utils import assert_array_equal, datasets_dir
+from tensoralloy.test_utils import assert_array_equal, datasets_dir, test_dir
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
@@ -112,6 +114,47 @@ def test_check_hparams():
     assert_equal(hparams.loss.energy.weight, 3.0)
     assert_equal(hparams.loss.forces.weight, 1.0)
     assert_equal(hparams.loss.equivalently_trusted, False)
+
+
+def test_graph_model_variables():
+    """
+    Test the exported variables of `BasicNN.export`.
+    """
+    save_dir = join(test_dir(), "checkpoints", "qm7-k2")
+
+    graph = tf.Graph()
+    with graph.as_default():
+        saver = tf.train.import_meta_graph(
+            join(save_dir, "model.ckpt-10000.meta"))
+
+        with tf.Session() as sess:
+            saver.restore(sess, join(save_dir, "model.ckpt-10000"))
+            train_values = {}
+            for var in tf.trainable_variables():
+                train_values[var.op.name] = sess.run(
+                    graph.get_tensor_by_name(
+                        f"{var.op.name}/ExponentialMovingAverage:0"))
+
+
+    graph_path = join(save_dir, "qm7.pb")
+    graph = tf.Graph()
+
+    with graph.as_default():
+
+        output_graph_def = graph_pb2.GraphDef()
+        with open(graph_path, "rb") as fp:
+            output_graph_def.ParseFromString(fp.read())
+            importer.import_graph_def(output_graph_def, name="")
+
+        sess = tf.Session(
+            config=tf.ConfigProto(allow_soft_placement=True),
+            graph=graph)
+
+        with sess:
+            for var_op_name, ref_value in train_values.items():
+                value = sess.run(f"{var_op_name}:0")
+                dmax = np.abs(np.atleast_1d(value - ref_value)).max()
+                assert_equal(dmax, 0)
 
 
 if __name__ == "__main__":
