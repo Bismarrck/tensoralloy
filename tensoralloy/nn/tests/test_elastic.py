311cb372ba8e7e4a4c891cd60277421ca3fa421a
Bismarrck <Bismarrck@me.com>
2019-04-12 10:12:44 +0800

    Add a test for `get_elastic_constant_tensor_op`.

---
+++ tensoralloy/nn/tests/test_elastic.py
@@ -0,0 +1,56 @@
+#!coding=utf-8
+"""
+This module defines unit tests of the elastic constant module.
+"""
+from __future__ import print_function, absolute_import
+
+import tensorflow as tf
+import nose
+
+from tensorflow_estimator import estimator as tf_estimator
+from ase.build import bulk
+from nose.tools import assert_in, assert_almost_equal
+
+from tensoralloy.nn import EamAlloyNN
+from tensoralloy.transformer import EAMTransformer
+
+__author__ = 'Xin Chen'
+__email__ = 'Bismarrck@me.com'
+
+
+def test_elastic_constant_tensor_op():
+    """
+    Test the method `get_elastic_constant_tensor_op`.
+    """
+    graph = tf.Graph()
+    with graph.as_default():
+        elements = ['Ni']
+        rc = 6.0
+        export_properties = ['energy', 'forces', 'stress', 'elastic']
+        nn = EamAlloyNN(elements, "zjw04", export_properties=export_properties)
+        clf = EAMTransformer(rc, elements)
+        nn.attach_transformer(clf)
+        predictions = nn.build(clf.get_placeholder_features(),
+                               mode=tf_estimator.ModeKeys.PREDICT,
+                               verbose=True)
+
+        assert_in('elastic', predictions)
+
+        with tf.Session() as sess:
+            tf.global_variables_initializer().run()
+            atoms = bulk('Ni', cubic=True)
+            elastic_tensor = sess.run(predictions.elastic,
+                                      feed_dict=clf.get_feed_dict(atoms))
+
+        assert_almost_equal(elastic_tensor[0, 0], 247, delta=1)
+        assert_almost_equal(elastic_tensor[1, 1], 247, delta=1)
+        assert_almost_equal(elastic_tensor[2, 2], 247, delta=1)
+        assert_almost_equal(elastic_tensor[0, 1], 147, delta=1)
+        assert_almost_equal(elastic_tensor[0, 2], 147, delta=1)
+        assert_almost_equal(elastic_tensor[3, 3], 125, delta=1)
+        assert_almost_equal(elastic_tensor[4, 4], 125, delta=1)
+        assert_almost_equal(elastic_tensor[5, 5], 125, delta=1)
+
+
+if __name__ == "__main__":
+    nose.run()
