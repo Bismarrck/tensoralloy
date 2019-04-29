Fix the sign of `E_pv`.

tensoralloy/nn/basic.py
@@ -255,17 +255,20 @@ class BasicNN:
                              name='energy',
                              verbose=True) -> tf.Tensor:
         """
-        Return the Op to compute total energy.
+        Return the Op to compute total energy:
+
+            Internal Energy (E) = Ethalpy (H) - pressure * Volume
+
         """
         with tf.name_scope("Enthalpy"):
             enthalpy = self._get_enthalpy_op(
                 outputs=outputs,
                 features=features,
                 verbose=verbose)
-        with tf.name_scope("External"):
+        with tf.name_scope("PV"):
             pv = self._get_pv_energy_op(features, verbose=verbose)
 
-        return tf.add_n([enthalpy, pv], name=name)
+        return tf.subtract(enthalpy, pv, name=name)
 
     @staticmethod
     def _get_forces_op(energy, positions, name='forces', verbose=True):

tensoralloy/nn/tests/test_basic_nn.py
@@ -60,12 +60,12 @@ def test_energy_pv():
                     clf.get_constant_features(atoms), mode=mode, verbose=False)
 
             with tf.name_scope("10GPa"):
-                set_pulay_stress(atoms, -10.0 * GPa)
+                set_pulay_stress(atoms, 10.0 * GPa)
                 ten_ops = nn.build(
                     clf.get_constant_features(atoms), mode=mode, verbose=False)
 
             with tf.name_scope("100GPa"):
-                set_pulay_stress(atoms, -100.0 * GPa)
+                set_pulay_stress(atoms, 100.0 * GPa)
                 hundred_ops = nn.build(
                     clf.get_constant_features(atoms), mode=mode, verbose=False)
 