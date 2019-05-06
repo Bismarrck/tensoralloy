9e0491e2c2a8075a4ea033cdbda104b4e992817c
Bismarrck <Bismarrck@me.com>
2019-05-06 14:14:16 +0800

    Add a function `connect` as the wrapper of `ase.db.connect`.

--- tensoralloy/io/neighbor.py
+++ tensoralloy/io/neighbor.py
@@ -8,15 +8,20 @@ import numpy as np
 import json
 
 from ase.db.sqlite import SQLite3Database
+from ase.db import connect as ase_connect
 from ase.neighborlist import neighbor_list
+from ase.utils import PurePath
 from joblib import Parallel, delayed
 from collections import Counter
 from typing import Dict
+from os.path import splitext
 
 from tensoralloy.utils import cantor_pairing, nested_get, nested_set
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+__all__ = ["connect", "CoreDatabase"]
 
 
 def _get_keypath(k_max: int, rc: float, prop: str):
@@ -24,6 +29,53 @@ def _get_keypath(k_max: int, rc: float, prop: str):
     A helper function to get the corresponding keypath.
     """
     return f"{k_max}.{'{:.2f}'.format(np.round(rc, decimals=2))}.{prop}"
+
+
+def connect(name, use_lock_file=True, append=True, serial=False):
+    """
+    Create connection to database.
+
+    Parameters
+    ----------
+    name: str
+        Filename or address of database.
+    use_lock_file: bool
+        You can turn this off if you know what you are doing ...
+    append: bool
+        Use append=False to start a new database.
+    serial : bool
+        Let someone else handle parallelization.  Default behavior is to
+        interact with the database on the master only and then distribute
+        results to all slaves.
+
+    """
+    if isinstance(name, PurePath):
+        name = str(name)
+
+    if name is None:
+        db_type = None
+    elif not isinstance(name, str):
+        db_type = 'json'
+    elif (name.startswith('postgresql://') or
+          name.startswith('postgres://')):
+        db_type = 'postgresql'
+    else:
+        db_type = splitext(name)[1][1:]
+        if db_type == '':
+            raise ValueError('No file extension or database type given')
+
+    if db_type == 'db':
+        return CoreDatabase(name,
+                            create_indices=True,
+                            use_lock_file=use_lock_file,
+                            serial=serial)
+    else:
+        return ase_connect(name,
+                           type=db_type,
+                           create_indices=True,
+                           use_lock_file=use_lock_file,
+                           append=append,
+                           serial=serial)
 
 
 class CoreDatabase(SQLite3Database):
@@ -189,27 +241,6 @@ class CoreDatabase(SQLite3Database):
                 'nnl_max': values[2]}
 
 
-def convert_k_max_to_key(k_max):
-    """ Convert `k_max` to a valid key.  """
-    return "{}".format(k_max)
-
-
-def convert_rc_to_key(rc):
-    """ Convert `rc` to a valid key. """
-    return "{:.2f}".format(round(rc, 4))
-
-
-def read_neighbor_sizes(db: SQLite3Database, k_max, rc):
-    """
-    Read `nij_max`, `nijk_max` and `nnl_max` from a database given `k_max` and
-    `rc`.
-    """
-    rc = convert_rc_to_key(rc)
-    k_max = convert_k_max_to_key(k_max)
-    details = db.metadata["neighbors"][k_max][rc]
-    return details['nij_max'], details['nijk_max'], details['nnl_max']
-
-
 def find_neighbor_size_of_atoms(atoms, rc, k_max):
     """
     A helper function to find `nij`, `nijk` and `nnl` for the `Atoms` object.
@@ -249,89 +280,3 @@ def find_neighbor_size_of_atoms(atoms, rc, k_max):
     else:
         nijk = 0
     return nij, nijk, nnl
-
-
-def _get_updated_metadata(database: SQLite3Database, k_max: int, rc: float,
-                          nij_max: int, nijk_max: int, nnl_max: int):
-    """
-    Return the updated metadata dict.
-    """
-    k_max = convert_k_max_to_key(k_max)
-    rc = convert_rc_to_key(rc)
-    details = {k_max: {rc: {
-        'nij_max': nij_max, 'nijk_max': nijk_max, 'nnl_max': nnl_max}}}
-    metadata = database.metadata
-    if 'neighbors' not in metadata:
-        metadata['neighbors'] = details
-    elif k_max not in metadata['neighbors']:
-        metadata['neighbors'][k_max] = details[k_max]
-    else:
-        metadata['neighbors'][k_max][rc] = details[k_max][rc]
-    return metadata
-
-
-def find_neighbor_size_maximums(database: SQLite3Database,
-                                rc: float,
-                                k_max=3,
-                                n_jobs=-1,
-                                verbose=True):
-    """
-    Find `nij_max`, `nijk_max` and 'nnl_max' of all `Atoms` objects in the
-    database.
-
-    Parameters
-    ----------
-    database : SQLite3Database
-        The database to update. This db must be created by the function `read`.
-    rc : float
-        The cutoff radius.
-    k_max : int
-        The maximum k for the many-body expansion scheme.
-    n_jobs : int
-        The maximum number of concurrently running jobs. If -1 all CPUs are
-        used. If 1 is given, no parallel computing code is used at all, which is
-        useful for debugging. For n_jobs below -1, (n_cpus + 1 + n_jobs) are
-        used. Thus for n_jobs = -2, all CPUs but one are used.
-    verbose : bool
-        If True, the progress shall be logged.
-
-    """
-
-    def _find_wrapper(aid):
-        return find_neighbor_size_of_atoms(
-            database.get_atoms(f'id={aid}'), rc, k_max)
-
-    if verbose:
-        print('Start finding neighbors for rc = {} and k_max = {}. This may '
-              'take a very long time.'.format(rc, k_max))
-
-    verb_level = 5 if verbose else 0
-    results = Parallel(n_jobs=n_jobs, verbose=verb_level)(
-        delayed(_find_wrapper)(jid) for jid in range(1, len(database) + 1)
-    )
-
-    nij_max, nijk_max, nnl_max = np.asarray(
-        results, dtype=int).max(axis=0).tolist()
-
-    database.metadata = _get_updated_metadata(
-        database,
-        k_max=k_max,
-        rc=rc,
-        nij_max=nij_max,
-        nijk_max=nijk_max,
-        nnl_max=nnl_max)
-
-    if k_max == 3:
-        database.metadata = _get_updated_metadata(
-            database,
-            k_max=2,
-            rc=rc,
-            nij_max=nij_max,
-            nijk_max=0,
-            nnl_max=nnl_max)
-
-    if verbose:
-        print(f'All {len(database)} jobs are done. nij_max = {nij_max}, '
-              f'nijk_max = {nijk_max}, nnl_max = {nnl_max}')
-
-    return nij_max, nijk_max, nnl_max
