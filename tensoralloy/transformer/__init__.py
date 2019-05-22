6267a65562625f56e8a1f0b0053f2f34838b5c50
Bismarrck <Bismarrck@me.com>
2019-05-22 16:36:19 +0800

    Minor update.

--- tensoralloy/transformer/__init__.py
+++ tensoralloy/transformer/__init__.py
@@ -8,6 +8,7 @@ from __future__ import print_function, absolute_import
 from tensoralloy.transformer.behler import BatchSymmetryFunctionTransformer
 from tensoralloy.transformer.behler import SymmetryFunctionTransformer
 from tensoralloy.transformer.eam import BatchEAMTransformer, EAMTransformer
+from tensoralloy.transformer.adp import BatchADPTransformer, ADPTransformer
 from tensoralloy.transformer.index_transformer import IndexTransformer
 
 __author__ = 'Xin Chen'
