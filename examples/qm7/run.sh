35bbd4e1a82c6080f52a582b882ad9259d649f20
Bismarrck <Bismarrck@me.com>
2019-08-26 23:14:08 +0800

    These two run.sh files are simplified.

--- examples/qm7/run.sh
+++ examples/qm7/run.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-work_dir=$(pwd)
-export PYTHONPATH=${work_dir}/../..:$PYTHONPATH
-export PATH=${work_dir}/../../tools:$PATH
-chmod +x "${work_dir}"/../../tools/tensoralloy
-
 if ! [[ -f qm7.db ]]; then
     tensoralloy build database qm7.extxyz --energy-unit=eV
 fi
--- examples/snap/run.sh
+++ examples/snap/run.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-work_dir=$(pwd)
-export PYTHONPATH=${work_dir}/../..:$PYTHONPATH
-export PATH=${work_dir}/../../tools:$PATH
-chmod +x "${work_dir}"/../../tools/tensoralloy
-
 tensoralloy run input.toml
 tensoralloy print train/logfile
