131fcd86df62bfb13145b4dbb24d3670ab863d3c
Bismarrck <Bismarrck@me.com>
2019-08-26 20:11:18 +0800

    The readme is updated.

--- examples/qm7/run.sh
+++ examples/qm7/run.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 work_dir=$(pwd)
 export PYTHONPATH=${work_dir}/../..:$PYTHONPATH
 export PATH=${work_dir}/../../tools:$PATH
 chmod +x "${work_dir}"/../../tools/tensoralloy
 
-if ! [ -f qm7.db ]; then
+if ! [[ -f qm7.db ]]; then
     tensoralloy build database qm7.extxyz --energy-unit=eV
 fi
 
 tensoralloy run input.toml
+tensoralloy print train/logfile
