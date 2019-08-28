4cada164ea8bca678a82f69e7dff889691b5ce85
Bismarrck <Bismarrck@me.com>
2019-08-28 15:19:09 +0800

    [Merge]: use absolute import path.

--- tensoralloy/io/input/__init__.py
+++ tensoralloy/io/input/__init__.py
@@ -4,7 +4,7 @@ This module defines the TOML input file reader.
 """
 from __future__ import print_function, absolute_import
 
-from .reader import InputReader
+from tensoralloy.io.input.reader import InputReader
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
