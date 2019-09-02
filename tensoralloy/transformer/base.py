f66d1df971f7a270096afb2ebb361d57ceba5168
Bismarrck <Bismarrck@me.com>
2019-09-02 11:00:47 +0800

    [Merge]: the base transformer classes are improved.

--- tensoralloy/transformer/base.py
+++ tensoralloy/transformer/base.py
@@ -14,9 +14,9 @@ from ase import Atoms
 from ase.units import GPa
 from ase.calculators.singlepoint import SinglePointCalculator
 
-from tensoralloy.utils import AttributeDict, get_pulay_stress
+from tensoralloy.utils import get_pulay_stress
 from tensoralloy.precision import get_float_dtype
-from tensoralloy.transformer.index_transformer import IndexTransformer
+from tensoralloy.transformer.vap import VirtualAtomMap
 from tensoralloy.transformer.indexed_slices import G2IndexedSlices
 
 
@@ -33,7 +33,7 @@ class BaseTransformer:
         """
         Initialization method.
         """
-        self._index_transformers = {}
+        self._vap_transformers = {}
 
     @property
     @abc.abstractmethod
@@ -45,9 +45,9 @@ class BaseTransformer:
 
     @property
     @abc.abstractmethod
-    def k_max(self):
+    def angular(self):
         """
-        Return the k-max.
+        Return if angular symmetry functions should be used or not.
         """
         pass
 
@@ -80,9 +80,9 @@ class BaseTransformer:
         pass
 
     @abc.abstractmethod
-    def get_index_transformer(self, atoms: Atoms):
+    def get_vap_transformer(self, atoms: Atoms):
         """
-        Return the corresponding `IndexTransformer`.
+        Return the corresponding `VirtualAtomMap`.
 
         Parameters
         ----------
@@ -91,26 +91,26 @@ class BaseTransformer:
 
         Returns
         -------
-        clf : IndexTransformer
-            The `IndexTransformer` for the given `Atoms` object.
+        vap : VirtualAtomMap
+            The `VirtualAtomMap` for the given `Atoms` object.
 
         """
         pass
 
     @abc.abstractmethod
-    def get_descriptors(self, features: AttributeDict):
+    def get_descriptors(self, features: dict):
         """
         An abstract method. Return the Op to compute atomic descriptors from raw
         properties (positions, volume, v2g_map, ...).
 
         Parameters
         ----------
-        features : AttributeDict
-            A dict of raw property tensors.
+        features : dict
+            A dict of features.
 
         Returns
         -------
-        descriptors : AttributeDict
+        descriptors : dict
             A dict of Ops to get atomic descriptors.
 
         """
@@ -127,7 +127,7 @@ class DescriptorTransformer(BaseTransformer):
         Initialization method.
         """
         super(DescriptorTransformer, self).__init__()
-        self._placeholders = AttributeDict()
+        self._placeholders = dict()
 
     @abc.abstractmethod
     def get_feed_dict(self, atoms: Atoms):
@@ -161,6 +161,37 @@ class DescriptorTransformer(BaseTransformer):
         """
         return getattr(self, "build_graph")(features)
 
+    @staticmethod
+    def _get_or_create_placeholder(dtype, shape, name):
+        """
+        Get a placeholder tensor from the graph or just create a new one.
+        """
+        try:
+            return tf.get_default_graph().get_tensor_by_name(
+                f'Placeholders/{name}:0')
+        except KeyError:
+            return tf.placeholder(dtype, shape, name)
+        except Exception as excp:
+            raise excp
+
+    def _create_float(self, name, dtype):
+        return self._get_or_create_placeholder(dtype, (), name)
+
+    def _create_float_1d(self, name, dtype):
+        return self._get_or_create_placeholder(dtype, (None,), name)
+
+    def _create_float_2d(self, dtype, d1, name, d0=None):
+        return self._get_or_create_placeholder(dtype, (d0, d1), name)
+
+    def _create_int(self, name):
+        return self._get_or_create_placeholder(tf.int32, (), name)
+
+    def _create_int_1d(self, name, d0=None):
+        return self._get_or_create_placeholder(tf.int32, (d0,), name)
+
+    def _create_int_2d(self, d1, name, d0=None):
+        return self._get_or_create_placeholder(tf.int32, (d0, d1), name)
+
     @abc.abstractmethod
     def _initialize_placeholders(self):
         """
@@ -176,9 +207,9 @@ class DescriptorTransformer(BaseTransformer):
             self._initialize_placeholders()
         return self._placeholders
 
-    def get_index_transformer(self, atoms: Atoms):
+    def get_vap_transformer(self, atoms: Atoms):
         """
-        Return the corresponding `IndexTransformer`.
+        Return the corresponding `VirtualAtomMap`.
 
         Parameters
         ----------
@@ -187,23 +218,23 @@ class DescriptorTransformer(BaseTransformer):
 
         Returns
         -------
-        clf : IndexTransformer
-            The `IndexTransformer` for the given `Atoms` object.
+        vap : VirtualAtomMap
+            The `VirtualAtomMap` for the given `Atoms` object.
 
         """
         # The mode 'reduce' is important here because chemical symbol lists of
         # ['C', 'H', 'O'] and ['C', 'O', 'H'] should be treated differently!
         formula = atoms.get_chemical_formula(mode='reduce')
-        if formula not in self._index_transformers:
+        if formula not in self._vap_transformers:
             symbols = atoms.get_chemical_symbols()
             max_occurs = Counter()
             counter = Counter(symbols)
             for element in self.elements:
                 max_occurs[element] = max(1, counter[element])
-            self._index_transformers[formula] = IndexTransformer(
+            self._vap_transformers[formula] = VirtualAtomMap(
                 max_occurs, symbols
             )
-        return self._index_transformers[formula]
+        return self._vap_transformers[formula]
 
 
 def bytes_feature(value):
@@ -225,17 +256,6 @@ def int64_feature(value):
     Convert the `value` to Protobuf int64.
     """
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _get_confidence_vector(atoms: Atoms) -> np.ndarray:
-    """
-    Return a vector as the confidences of energy, forces and stress of this
-    `Atoms`.
-    """
-    if 'data' in atoms.info:
-        return atoms.info['data'].get("weights", np.ones(3))
-    else:
-        return atoms.info.get("weights", np.ones(3))
 
 
 class BatchDescriptorTransformer(BaseTransformer):
@@ -328,9 +348,9 @@ class BatchDescriptorTransformer(BaseTransformer):
         """
         pass
 
-    def get_index_transformer(self, atoms: Atoms):
+    def get_vap_transformer(self, atoms: Atoms):
         """
-        Return the corresponding `IndexTransformer`.
+        Return the corresponding `VirtualAtomMap`.
 
         Parameters
         ----------
@@ -339,48 +359,34 @@ class BatchDescriptorTransformer(BaseTransformer):
 
         Returns
         -------
-        clf : IndexTransformer
-            The `IndexTransformer` for the given `Atoms` object.
+        vap : VirtualAtomMap
+            The `VirtualAtomMap` for the given `Atoms` object.
 
         """
         # The mode 'reduce' is important here because chemical symbol lists of
         # ['C', 'H', 'O'] and ['C', 'O', 'H'] should be treated differently!
         formula = atoms.get_chemical_formula(mode='reduce')
-        if formula not in self._index_transformers:
-            self._index_transformers[formula] = IndexTransformer(
+        if formula not in self._vap_transformers:
+            self._vap_transformers[formula] = VirtualAtomMap(
                 self.max_occurs, atoms.get_chemical_symbols()
             )
-        return self._index_transformers[formula]
-
-    def _resize_to_nij_max(self, alist: np.ndarray, is_indices=True):
-        """
-        A helper function to resize the given array.
-        """
-        if np.ndim(alist) == 1:
-            shape = [self.nij_max, ]
-        else:
-            shape = [self.nij_max, ] + list(alist.shape[1:])
-        nlist = np.zeros(shape, dtype=np.int32)
-        length = len(alist)
-        nlist[:length] = alist
-        if is_indices:
-            nlist[:length] += 1
-        return nlist
+        return self._vap_transformers[formula]
 
     @staticmethod
     def _encode_g2_indexed_slices(g2: G2IndexedSlices):
         """
         Encode the indexed slices of G2:
             * `v2g_map`, `ilist` and `jlist` are merged into a single array
-              with key 'r_indices'.
-            * `shift` will be encoded separately with key 'r_shifts'.
+              with key 'g2.indices'.
+            * `n1` will be encoded separately with key 'g2.shifts'.
 
         """
         indices = np.concatenate((
-            g2.v2g_map, g2.ilist[..., np.newaxis], g2.jlist[..., np.newaxis],
-        ), axis=1).tostring()
-        return {'g2.indices': bytes_feature(indices),
-                'g2.shifts': bytes_feature(g2.shift.tostring())}
+            g2.v2g_map,
+            g2.ilist[..., np.newaxis],
+            g2.jlist[..., np.newaxis]), axis=1)
+        return {'g2.indices': bytes_feature(indices.tostring()),
+                'g2.shifts': bytes_feature(g2.n1.tostring())}
 
     def _encode_atoms(self, atoms: Atoms) -> dict:
         """
@@ -394,16 +400,14 @@ class BatchDescriptorTransformer(BaseTransformer):
                           'stress': np.zeros(6)})
             atoms.calc = sp
 
-        clf = self.get_index_transformer(atoms)
+        vap = self.get_vap_transformer(atoms)
         np_dtype = get_float_dtype().as_numpy_dtype
-        positions = clf.map_positions(atoms.positions).astype(np_dtype)
+        positions = vap.map_positions(atoms.positions).astype(np_dtype)
         cells = atoms.get_cell(complete=True).array.astype(np_dtype)
         volume = np.atleast_1d(atoms.get_volume()).astype(np_dtype)
         y_true = np.atleast_1d(atoms.get_total_energy()).astype(np_dtype)
         composition = self._get_composition(atoms)
-        mask = clf.mask.astype(np_dtype)
-        confidences = np.reshape(
-            _get_confidence_vector(atoms).astype(np_dtype), (3, 1))
+        mask = vap.atom_masks.astype(np_dtype)
         pulay = np.atleast_1d(get_pulay_stress(atoms)).astype(np_dtype)
 
         feature_list = {
@@ -412,15 +416,13 @@ class BatchDescriptorTransformer(BaseTransformer):
             'n_atoms': int64_feature(len(atoms)),
             'volume': bytes_feature(volume.tostring()),
             'y_true': bytes_feature(y_true.tostring()),
-            'y_conf': bytes_feature(confidences[0].tostring()),
             'mask': bytes_feature(mask.tostring()),
             'composition': bytes_feature(composition.tostring()),
             'pulay': bytes_feature(pulay.tostring()),
         }
         if self.use_forces:
-            f_true = clf.map_forces(atoms.get_forces()).astype(np_dtype)
+            f_true = vap.map_forces(atoms.get_forces()).astype(np_dtype)
             feature_list['f_true'] = bytes_feature(f_true.tostring())
-            feature_list['f_conf'] = bytes_feature(confidences[1].tostring())
 
         if self.use_stress:
             # Convert the unit of the stress tensor to 'eV' for simplification:
@@ -433,7 +435,6 @@ class BatchDescriptorTransformer(BaseTransformer):
             feature_list['stress'] = bytes_feature(virial.tostring())
             feature_list['total_pressure'] = bytes_feature(
                 np.atleast_1d(internal_pressure).tostring())
-            feature_list['s_conf'] = bytes_feature(confidences[2].tostring())
         return feature_list
 
     @abc.abstractmethod
@@ -453,94 +454,82 @@ class BatchDescriptorTransformer(BaseTransformer):
                       max_n_atoms: int,
                       n_elements: int,
                       use_forces=True,
-                      use_stress=False) -> AttributeDict:
+                      use_stress=False) -> dict:
         """
         Decode `Atoms` related properties.
         """
-        decoded = AttributeDict()
+        decoded = dict()
 
         length = 3 * (max_n_atoms + 1)
         float_dtype = get_float_dtype()
 
         positions = tf.decode_raw(example['positions'], float_dtype)
         positions.set_shape([length])
-        decoded.positions = tf.reshape(
+        decoded["positions"] = tf.reshape(
             positions, (max_n_atoms + 1, 3), name='R')
 
         n_atoms = tf.identity(example['n_atoms'], name='n_atoms')
-        decoded.n_atoms = n_atoms
+        decoded["n_atoms"] = n_atoms
 
         y_true = tf.decode_raw(example['y_true'], float_dtype)
         y_true.set_shape([1])
-        decoded.y_true = tf.squeeze(y_true, name='y_true')
-
-        y_conf = tf.decode_raw(example['y_conf'], float_dtype)
-        y_conf.set_shape([1])
-        decoded.y_conf = tf.squeeze(y_conf, name='y_conf')
+        decoded["y_true"] = tf.squeeze(y_true, name='y_true')
 
         cells = tf.decode_raw(example['cells'], float_dtype)
         cells.set_shape([9])
-        decoded.cells = tf.reshape(cells, (3, 3), name='cells')
+        decoded["cells"] = tf.reshape(cells, (3, 3), name='cells')
 
         volume = tf.decode_raw(example['volume'], float_dtype)
         volume.set_shape([1])
-        decoded.volume = tf.squeeze(volume, name='volume')
+        decoded["volume"] = tf.squeeze(volume, name='volume')
 
         mask = tf.decode_raw(example['mask'], float_dtype)
         mask.set_shape([max_n_atoms + 1, ])
-        decoded.mask = mask
+        decoded["atom_masks"] = mask
 
         composition = tf.decode_raw(example['composition'], float_dtype)
         composition.set_shape([n_elements, ])
-        decoded.composition = composition
+        decoded["composition"] = composition
 
         pulay = tf.decode_raw(example['pulay'], float_dtype)
         pulay.set_shape([1])
-        decoded.pulay_stress = tf.squeeze(pulay, name='pulay_stress')
+        decoded["pulay_stress"] = tf.squeeze(pulay, name='pulay_stress')
 
         if use_forces:
             f_true = tf.decode_raw(example['f_true'], float_dtype)
             # Ignore the forces of the virtual atom
             f_true.set_shape([length, ])
-            decoded.f_true = tf.reshape(
+            decoded["f_true"] = tf.reshape(
                 f_true, (max_n_atoms + 1, 3), name='f_true')
-
-            f_conf = tf.decode_raw(example['f_conf'], float_dtype)
-            f_conf.set_shape([1])
-            decoded.f_conf = tf.squeeze(f_conf, name='f_conf')
 
         if use_stress:
             stress = tf.decode_raw(
                 example['stress'], float_dtype, name='stress')
             stress.set_shape([6])
-            decoded.stress = stress
+            decoded["stress"] = stress
 
             total_pressure = tf.decode_raw(
                 example['total_pressure'], float_dtype)
             total_pressure.set_shape([1])
-            decoded.total_pressure = tf.squeeze(
+            decoded["total_pressure"] = tf.squeeze(
                 total_pressure, name='total_pressure')
-
-            s_conf = tf.decode_raw(example['s_conf'], float_dtype)
-            s_conf.set_shape([1])
-            decoded.s_conf = tf.squeeze(s_conf, name='s_conf')
 
         return decoded
 
     @abc.abstractmethod
-    def decode_protobuf(self, example_proto: tf.Tensor) -> AttributeDict:
+    def decode_protobuf(self, example_proto: tf.Tensor) -> dict:
         """
         Decode the scalar string Tensor, which is a single serialized Example.
         See `_parse_single_example_raw` documentation for more details.
         """
         pass
 
-    def _infer_batch_size(self, batch_raw_properties: AttributeDict):
+    def _infer_batch_size(self, next_batch: dict):
         """
-        Infer `batch_size` from `batch_raw_properties`.
+        Infer `batch_size` from `next_batch`.
         """
 
-        for name, tensor in batch_raw_properties.items():
+        for name, tensor in next_batch.items():
             if isinstance(tensor, tf.Tensor):
                 batch_size = tensor.shape[0].value
             elif isinstance(tensor, np.ndarray):
@@ -557,19 +546,19 @@ class BatchDescriptorTransformer(BaseTransformer):
         self._batch_size = batch_size
 
     @abc.abstractmethod
-    def get_descriptors(self, batch_features: AttributeDict):
+    def get_descriptors(self, next_batch: dict):
         """
         An abstract method. Return the Op to compute atomic descriptors from raw
         properties (positions, volume, v2g_map, ...).
 
         Parameters
         ----------
-        batch_features : AttributeDict
+        next_batch : dict
             A dict of batched raw property tensors.
 
         Returns
         -------
-        descriptors : AttributeDict
+        descriptors : dict
             A dict of Ops to get atomic descriptors.
 
         """
