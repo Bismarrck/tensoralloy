5070f4733e2efeab2462a5a567f693dfd8b04604
Bismarrck <Bismarrck@me.com>
2019-09-05 13:54:15 +0800

    Fix the module inits.

--- tensoralloy/nn/__init__.py
+++ tensoralloy/nn/__init__.py
@@ -4,8 +4,14 @@
 """
 from __future__ import print_function, absolute_import
 
-from tensoralloy.nn.atomic import AtomicResNN, AtomicNN
-from tensoralloy.nn.eam import EamAlloyNN, EamFsNN, AdpNN
+from tensoralloy.nn.atomic.resnet import AtomicResNN
+from tensoralloy.nn.atomic.atomic import AtomicNN
+from tensoralloy.nn.eam.alloy import EamAlloyNN
+from tensoralloy.nn.eam.fs import EamFsNN
+from tensoralloy.nn.eam.adp import AdpNN
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+__all__ = ["AtomicResNN", "AtomicNN",
+           "EamAlloyNN", "EamFsNN", "AdpNN"]
--- tensoralloy/train/__init__.py
+++ tensoralloy/train/__init__.py
@@ -4,7 +4,7 @@ This module defines functions and classes for training a model.
 """
 from __future__ import print_function, absolute_import
 
-from .training import TrainingManager
+from tensoralloy.train.training import TrainingManager
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
