433a9c84063f0faaa7a492023e9c2fbffb71eacb
Xin Chen <Bismarrck@me.com>
2020-07-10 15:53:27 +0800

    Register `SymmetryFunctionNN` and `GRAP`.

--- tensoralloy/nn/atomic/__init__.py
+++ tensoralloy/nn/atomic/__init__.py
@@ -6,6 +6,11 @@ from __future__ import print_function, absolute_import
 
 from tensoralloy.nn.atomic.atomic import AtomicNN
 from tensoralloy.nn.atomic.deepmd import DeepPotSE
+from tensoralloy.nn.atomic.sf import SymmetryFunctionNN
+from tensoralloy.nn.atomic.generic import GenericRadialAtomicPotential
 
 __author__ = 'Xin Chen'
 __email__ = 'Bismarrck@me.com'
+
+__all__ = ["AtomicNN", "DeepPotSE", "SymmetryFunctionNN", 
+           "GenericRadialAtomicPotential"]
